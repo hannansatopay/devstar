1a82eb86b05b17e9ccc12ef857832810b94d64d9
MRPrayh <80837351+MRPrayh@users.noreply.github.com>
2023-09-29 11:08:51 +0530

    Create new route in routes\(tools)\online-spreadsheet\

---
+++ src/routes/(tools)/online-spreadsheet/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import Intro from '$lib/Intro.svelte';
+	import { Label, Input, Range, Radio, Checkbox } from 'flowbite-svelte';
+	import { onMount } from 'svelte';
+
+	export let data;
+
+</script>
+
+<Intro heading={data.meta.title} description={data.meta.description} />
+
+<svelte:window bind:innerWidth bind:innerHeight />
+
+<section class="bg-white dark:bg-gray-900">
+	<div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
+		<div class="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-4 md:gap-12 md:space-y-0">
+			//Code goes here
+		</div>
+	</div>
+</section>
