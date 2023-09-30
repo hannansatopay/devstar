21e17db9b3f4d695218ee18144d31776733ffc34
rex9123 <114596205+rex9123@users.noreply.github.com>
2023-09-30 23:24:38 +0530

    Centering the buttons

--- src/routes/(tools)/online-spreadsheet/+page.svelte
+++ src/routes/(tools)/online-spreadsheet/+page.svelte
@@ -17,21 +17,20 @@
 
 <svelte:window bind:innerWidth bind:innerHeight />
 
-<section class="bg-white dark:bg-gray-900">
+<section class="bg-white dark:bg-gray-800">
     <div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
-        <div class="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-4 md:gap-12 md:space-y-0 md: space-x-0">
-            <div class="flex justify-center items-center">
-                <button class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-5 px-5 rounded">
+        <div class="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-4 md:gap-12 md:space-y-0">
+            <div class="flex justify-center  ml-80">
+                <button class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-5 px-20 rounded">
                     Open New Spreadsheet (.csv)
                 </button>
             </div>
-            <div class="flex justify-center items-center">
-                <button class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-5 px-5 rounded">
+            <div class="flex justify-center  ml-80">
+                <button class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-5 px-20 rounded">
                     Create New Spreadsheet (.csv)
                 </button>
             </div>
         </div>
     </div>
 </section>
-
 
