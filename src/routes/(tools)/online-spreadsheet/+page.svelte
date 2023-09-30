87f8edf4f93f187227ee57b0d18612c33348fe36
MRPrayh <80837351+MRPrayh@users.noreply.github.com>
2023-09-30 09:44:48 +0530

    Defined innerWidth in +page.svelte
    Defined innerWidth and other screen parameters which solves the error and now all the Elements on the page are visible.

--- src/routes/(tools)/online-spreadsheet/+page.svelte
+++ src/routes/(tools)/online-spreadsheet/+page.svelte
@@ -1,20 +1,34 @@
 <script lang="ts">
 	import Intro from '$lib/Intro.svelte';
+	import { Button, GradientButton } from 'flowbite-svelte';
+	// import { EnvelopeSolid } from 'flowbite-svelte-icons';
 	import { Label, Input, Range, Radio, Checkbox } from 'flowbite-svelte';
 	import { onMount } from 'svelte';
 
 	export let data;
 
+	// Might need these for tabular view
+	let innerWidth: any = null; // defining this solve the of svelte:window tag  
+	let innerHeight: any = null; // and now all other elements are visible
+	let screenWidth: any = null;
+	let screenHeight: any = null;
+	
 </script>
 
 <Intro heading={data.meta.title} description={data.meta.description} />
 
-<!--<svelte:window bind:innerWidth bind:innerHeight /> -->
+<svelte:window bind:innerWidth bind:innerHeight />
 
 <section class="bg-white dark:bg-gray-900">
 	<div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
 		<div class="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-4 md:gap-12 md:space-y-0">
 			//Code goes here
+			<!-- <div class="flex flex-wrap gap-2"> -->
+			<!-- <Button size="xl" outline color="green">Open New Spreadsheet (.csv)</Button> -->
+			<GradientButton size="xl" color="blue">Open New Spreadsheet (.csv)</GradientButton>
+			<Button size="xl" outline color="blue">Open New Spreadsheet (.csv)</Button>
+				<!--<Button size="xl"><EnvelopeSolid class="w-4 h-4 mr-2" />Extra large</Button>-->
+			<!-- </div> -->
 		</div>
 	</div>
 </section>
