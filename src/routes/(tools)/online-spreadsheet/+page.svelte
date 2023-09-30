425138bbb453571d542efceb456c775ce45dd9c8
MRPrayh <80837351+MRPrayh@users.noreply.github.com>
2023-09-30 13:40:32 +0530

    Update +page.svelte
    aligned two buttons to the center while also keeping the previous code as comments if needed

--- src/routes/(tools)/online-spreadsheet/+page.svelte
+++ src/routes/(tools)/online-spreadsheet/+page.svelte
@@ -12,6 +12,12 @@
 	let innerHeight: any = null; // and now all other elements are visible
 	let screenWidth: any = null;
 	let screenHeight: any = null;
+
+	// for handling csv files
+	// let fileInput;
+	// let string = '';
+
+
 	
 </script>
 
@@ -20,15 +26,22 @@
 <svelte:window bind:innerWidth bind:innerHeight />
 
 <section class="bg-white dark:bg-gray-900">
-	<div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
-		<div class="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-4 md:gap-12 md:space-y-0">
-			//Code goes here
-			<!-- <div class="flex flex-wrap gap-2"> -->
-			<!-- <Button size="xl" outline color="green">Open New Spreadsheet (.csv)</Button> -->
-			<Button size="xl" outline color="blue">Upload Spreadsheet (.csv)</Button>
-			<GradientButton size="xl" color="blue">Create New Spreadsheet (.csv)</GradientButton>
-			<!-- <Button size="xl"><EnvelopeSolid class="w-4 h-4 mr-2" />Create New Spreadsheet (.csv)</Button> -->
+	<!-- <div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6"> -->
+		<!-- <div class="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-4 md:gap-12 md:space-y-0"> -->
+			<!-- <p></p> -->
+			<!-- <div> -->
+			
+			<!-- <Button size="xl" outline color="blue" on:click={()=>fileInput.click()} type="button">Upload Spreadsheet (.csv)</Button>
+			<input type="file" class="hidden" accept=".csv" bind:this={fileInput} on:change={handleFileInputChange}> -->
+
+			
+			<!--<Button size="xl"><EnvelopeSolid class="w-4 h-4 mr-2" />Create New Spreadsheet (.csv)</Button> -->
 			<!-- </div> -->
-		</div>
+			<!-- <p></p> -->
+		<!-- </div> -->
+	<!-- </div> -->
+	<div class="flex justify-center items-center">
+		<Button size="xl" outline color="blue" class="py-2 px-4 m-6">Open New Spreadsheet (.csv)</Button>
+	    <GradientButton size="xl" color="blue" class="py-2 px-4 m-6">Create New Spreadsheet (.csv)</GradientButton>
 	</div>
 </section>
