e5399ace600ea8bf3681820e514fce27d8ab3cf1
Abhishek-2004-17 <abhisheksdaivathinveettil@gmail.com>
2023-09-30 21:52:26 +0530

    commit

---
+++ src/routes/(tools)/Amortization-Calculator/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+    let start=1000;
+    let year = 15;
+</script>
+<h1>Amortization Calculator</h1>
+<form action="">
+    Loan Amount<input type="number" class="sign" bind:value={start}>
+    Loan Term<input type="number"  bind:value={year} placeholder="Year">
+    <input type="number" placeholder="month">
+</form>
+<style>
+    h1{
+        color: blue;
+        text-align: center;
+    }
+    form{
+        padding: 4%;
+        text-align: center;
+    }
+</style>
