363682672d0f08413f254eebacbb759707ff15a9
MRPrayh <80837351+MRPrayh@users.noreply.github.com>
2023-09-30 23:03:16 +0530

    Two button's UI added from flowbite-svelte.

--- src/routes/(tools)/online-spreadsheet/+page.svelte
+++ src/routes/(tools)/online-spreadsheet/+page.svelte
@@ -1,24 +1,18 @@
 <script lang="ts">
 	import Intro from '$lib/Intro.svelte';
 	import { Button, GradientButton } from 'flowbite-svelte';
-	// import { EnvelopeSolid } from 'flowbite-svelte-icons';
-	import { Label, Input, Range, Radio, Checkbox } from 'flowbite-svelte';
-	import { onMount } from 'svelte';
+	
 
 	export let data;
 
 	// Might need these for tabular view
-	let innerWidth: any = null; // defining this solve the of svelte:window tag  
+	let innerWidth: any = null; // defining this solve the error of svelte:window tag  
 	let innerHeight: any = null; // and now all other elements are visible
 	let screenWidth: any = null;
 	let screenHeight: any = null;
 
-	// for handling csv files
-	// let fileInput;
-	// let string = '';
 
 
-	
 </script>
 
 <Intro heading={data.meta.title} description={data.meta.description} />
@@ -26,21 +20,7 @@
 <svelte:window bind:innerWidth bind:innerHeight />
 
 <section class="bg-white dark:bg-gray-900">
-	<!-- <div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6"> -->
-		<!-- <div class="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-4 md:gap-12 md:space-y-0"> -->
-			<!-- <p></p> -->
-			<!-- <div> -->
-			
-			<!-- <Button size="xl" outline color="blue" on:click={()=>fileInput.click()} type="button">Upload Spreadsheet (.csv)</Button>
-			<input type="file" class="hidden" accept=".csv" bind:this={fileInput} on:change={handleFileInputChange}> -->
-
-			
-			<!--<Button size="xl"><EnvelopeSolid class="w-4 h-4 mr-2" />Create New Spreadsheet (.csv)</Button> -->
-			<!-- </div> -->
-			<!-- <p></p> -->
-		<!-- </div> -->
-	<!-- </div> -->
-	<div class="flex justify-center items-center">
+	<div class="flex justify-center items-center"> <!--Two buttons added from flowbite-svelte-->
 		<Button size="xl" outline color="blue" class="m-6">Open New Spreadsheet<br/>(.csv)</Button>
 	    <GradientButton size="xl" color="blue" class="m-6">Create New Spreadsheet<br/>(.csv)</GradientButton>
 	</div>
