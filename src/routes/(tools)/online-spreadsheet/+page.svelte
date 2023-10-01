e95947c52be0fca93b45dcbbd4fbefda21fb2b7e
rex9123 <114596205+rex9123@users.noreply.github.com>
2023-10-01 11:32:17 +0530

    Final Button Centering Commit

--- src/routes/(tools)/online-spreadsheet/+page.svelte
+++ src/routes/(tools)/online-spreadsheet/+page.svelte
@@ -19,17 +19,13 @@
 
 <section class="bg-white dark:bg-gray-800">
     <div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
-        <div class="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-4 md:gap-12 md:space-y-0">
-            <div class="flex justify-center  ml-80">
-                <button class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-5 px-20 rounded">
-                    Open New Spreadsheet (.csv)
-                </button>
-            </div>
-            <div class="flex justify-center  ml-80">
-                <button class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-5 px-20 rounded">
-                    Create New Spreadsheet (.csv)
-                </button>
-            </div>
+        <div class="flex justify-center space-x-4">
+            <button class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-5 px-20 rounded">
+                Open New Spreadsheet (.csv)
+            </button>
+            <button class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-5 px-20 rounded">
+                Create New Spreadsheet (.csv)
+            </button>
         </div>
     </div>
 </section>
