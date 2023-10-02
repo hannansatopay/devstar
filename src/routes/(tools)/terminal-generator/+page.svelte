fc129edc5948130811915073e5f58c90ddddf661
Yash Nawale <98093480+Yash-2026@users.noreply.github.com>
2023-10-02 14:53:53 +0530

    Update +page.svelte

--- src/routes/(tools)/terminal-generator/+page.svelte
+++ src/routes/(tools)/terminal-generator/+page.svelte
@@ -2,14 +2,17 @@
 	import Intro from '$lib/Intro.svelte';
 	import { Label, Input, Range, Radio, Checkbox } from 'flowbite-svelte';
 	import { onMount } from 'svelte';
-
+	
 	export let data;
 </script>
 
-<!-- <Intro heading={data.meta.title} description={data.meta.description} /> -->
+{#if data.meta}
+		<Intro heading={data.meta.title} description={data.meta.description} />
+{/if}
 
 <section class="bg-white dark:bg-gray-900">
 	<div class="py-8 px-4 mx-auto max-w-screen-xl lg:px-12">
+		
 		<div class="card p-8 relative items-center mx-auto max-w-screen-xl overflow-hidden rounded-lg">
 			
 		</div>
@@ -25,3 +28,5 @@
 		box-shadow: rgba(255, 255, 255, 0.5) 0 0 0 2px;
 	}
 </style>
+
+
