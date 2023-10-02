751a4da1c2045e6269f0c32c7e25c8bde9db931d
Abdur Rehman Shaikh <abdurrahman19702@gmail.com>
2023-10-02 22:42:25 +0530

    Added Copy and Download Functionality

--- src/routes/(tools)/palindrome-generator/+page.svelte
+++ src/routes/(tools)/palindrome-generator/+page.svelte
@@ -1,16 +1,52 @@
 <script lang="ts">
-	import { Textarea, Label } from 'flowbite-svelte';
+	import { Textarea } from 'flowbite-svelte';
+	import { Button } from 'flowbite-svelte';
+	import jsPDF from 'jspdf';
 	import Intro from '$lib/Intro.svelte';
 
 	export let data;
 
-	var palindrome="";
+	var palindrome;
 
 	function generatePalindrome(input){
 		palindrome = input.target.value;
 		for (var i = input.target.value.length - 1; i >= 0; i--) {
                 palindrome += input.target.value[i];
             }
+	}
+
+	function copyText() {
+		if (palindrome.length > 0) {
+			var textarea = document.createElement("textarea");
+			textarea.value = palindrome;
+			document.body.appendChild(textarea);
+			textarea.select();
+			document.execCommand("copy");
+			document.body.removeChild(textarea);
+		}
+	}
+
+	function downloadText() {
+		if (palindrome.length > 0) {
+			var filename = "DevStarPalindrome.txt";
+			var blob = new Blob([palindrome], { type: 'text/plain' });
+			var url = window.URL.createObjectURL(blob);
+			
+			var a = document.createElement('a');
+			a.href = url;
+			a.download = filename;
+			document.body.appendChild(a);
+			a.click();
+			
+			window.URL.revokeObjectURL(url);
+			document.body.removeChild(a);
+		}
+	}
+  
+	function downloadPDF() {
+		const doc = new jsPDF();
+		doc.text(palindrome, 20, 20);
+		doc.save('DevStarPalindrome.pdf');
 	}
 
 </script>
@@ -20,20 +56,27 @@
 <section class="bg-white dark:bg-gray-900">
 	<div class="py-8 px-4 mx-auto max-w-screen-xl lg:px-12">
 		<div class="card p-8 relative items-center mx-auto max-w-screen-xl overflow-hidden rounded-lg">
-		
 			<div class="mt-3 gap-2 items-center mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 overflow-hidden">
+
 				<div class="rounded-lg overflow-hidden bg-gray-50 border border-gray-300" id="tarea1">
-					<Textarea placeholder="Enter Text" id="textarea-id" rows="8" name="message"
+					<Textarea placeholder="Enter Text" id="textarea-id" rows="8" name="message" 
 					on:input={generatePalindrome}/>
-				
 				</div>
+
 				<div class="rounded-lg overflow-hidden bg-gray-50 border border-gray-300" id="tarea2">
 					<Textarea placeholder="Result" id="textarea-id" rows="8" name="message" 
 					bind:value={palindrome}/>
-
 				</div>
+
 			</div>
 
+			<div id="buttonArea">
+				<Button color="blue" on:click={copyText}>Copy</Button>
+				<Button color="blue" on:click={downloadText}>Download as txt</Button>
+				<Button color="blue" on:click={downloadPDF}>Download as pdf</Button>
+			</div>
+
+		</div>
 	</div>
 </section>
 
@@ -45,6 +88,14 @@
 
 	#tarea2{
 		margin-left: 10px;
+	}
+
+	#buttonArea {
+		margin-top: 30px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		gap: 30px; /* Adjust the gap between buttons as needed */
 	}
 
 	.card {
