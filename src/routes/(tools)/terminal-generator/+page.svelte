ca4eb9ba176b12414f208e3cb9eb14e0f1ee5a8b
Yash Nawale <98093480+Yash-2026@users.noreply.github.com>
2023-10-02 15:08:28 +0530

    Added title and description from meta data

--- src/routes/(tools)/terminal-generator/+page.svelte
+++ src/routes/(tools)/terminal-generator/+page.svelte
@@ -2,14 +2,19 @@
 	import Intro from '$lib/Intro.svelte';
 	import { Label, Input, Range, Radio, Checkbox } from 'flowbite-svelte';
 	import { onMount } from 'svelte';
-
+	
+	
 	export let data;
 </script>
 
-<!-- <Intro heading={data.meta.title} description={data.meta.description} /> -->
+{#if data.meta}
+		<Intro heading={data.meta.title} description={data.meta.description} />
+{/if}
 
 <section class="bg-white dark:bg-gray-900">
 	<div class="py-8 px-4 mx-auto max-w-screen-xl lg:px-12">
+
+		<!-- <h1 class="text-3xl font-bold text-gray-900 dark:text-gray-100 ">Terminal Generator</h1> -->
 		<div class="card p-8 relative items-center mx-auto max-w-screen-xl overflow-hidden rounded-lg">
 			
 		</div>
@@ -25,3 +30,5 @@
 		box-shadow: rgba(255, 255, 255, 0.5) 0 0 0 2px;
 	}
 </style>
+
+
