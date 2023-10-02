5652a0033d664839dbc67aa9d335df32e8313345
GAUTAM MANDAL <95901992+Abhieo07@users.noreply.github.com>
2023-10-02 06:41:05 +0000

    New feature chart-generator is added.

--- src/routes/tools.js
+++ src/routes/tools.js
@@ -94,5 +94,11 @@ export let tools = {
         icon: 'spider-web',
         link: '/robots-txt-generator',
         description: "The Robots.txt Generator is a must-have tool for developers to create the robots.txt file, which controls search engine crawlers' access to specific pages or directories on a website. This tool offers a user-friendly interface to define crawling rules, including allow and disallow directives, to ensure optimal search engine indexing and prevent unwanted content from being indexed. Create and customize your robots.txt file efficiently with this generator to enhance your website's search engine optimization (SEO) efforts."
+    },
+    'chart-generator': {
+        name: 'Chart generator',
+        icon: 'charts',
+        link: '/chart-generator',
+        description: "Our chart generator tool empowers you to transform raw data into compelling visual insights. Customize, analyze, and share dynamic charts effortlessly, enhancing your data-driven decision-making process."
     }
 };
