509a9579f76752653f36d5d453dac16ee1be4d03
Jyotiraditya Patil <patiljyotiraditya123@gmail.com>
2023-10-02 19:26:55 +0530

    Tools upadte

--- src/routes/tools.js
+++ src/routes/tools.js
@@ -96,9 +96,9 @@ export let tools = {
         description: "The Robots.txt Generator is a must-have tool for developers to create the robots.txt file, which controls search engine crawlers' access to specific pages or directories on a website. This tool offers a user-friendly interface to define crawling rules, including allow and disallow directives, to ensure optimal search engine indexing and prevent unwanted content from being indexed. Create and customize your robots.txt file efficiently with this generator to enhance your website's search engine optimization (SEO) efforts."
     },
     'pdf-toolkit': {
-        name: 'pdf-toolkit',
+        name: 'PDF-ToolKit',
         icon: 'files',
         link: '/pdf-toolkit',
-        description:"pdf-toolkit"
+        description:"Pdf che je kahi kam astil ithe kra"
     }
 };
