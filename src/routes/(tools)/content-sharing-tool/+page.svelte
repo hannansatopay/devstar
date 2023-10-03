8a4e4dfd465d51cc708822758abd224ae81ab377
bandya2003 <smntjdhv@gmail.com>
2023-10-04 01:22:39 +0530

    Added the required module and also edited the tool.js file with asked changes

---
+++ src/routes/(tools)/content-sharing-tool/+page.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { Spinner } from 'flowbite-svelte';
+	import Intro from '$lib/Intro.svelte';
+  	
+	export let data;
+	let advice: string | null = null;
+
+	async function fetchData() {
+    	advice = null;
+		const response = await fetch('https://api.adviceslip.com/advice');
+		const result = await response.json();
+		advice = result['slip']['advice'];
+	}
+
+	onMount(async () => {
+		fetchData();
+	});
+</script>
+
+<Intro heading={data.meta.title} description={data.meta.description} />
+
+<section class="">
+	<div class="">
+    <div class="">
+	</div>
+</section>
