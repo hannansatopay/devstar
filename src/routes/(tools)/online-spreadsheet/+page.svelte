e92f2249e723fd303bf4a5e3e71b21de8b14800c
rex9123 <114596205+rex9123@users.noreply.github.com>
2023-10-03 19:10:39 +0530

    Open New Spreadsheet Config partially done

--- src/routes/(tools)/online-spreadsheet/+page.svelte
+++ src/routes/(tools)/online-spreadsheet/+page.svelte
@@ -1,70 +1,92 @@
 <script lang="ts">
-	import Tabulator from 'tabulator-tables';
-
+	import { Button, GradientButton } from 'flowbite-svelte';
 	import Intro from '$lib/Intro.svelte';
-	import { Label, Input, Range, Radio, Checkbox } from 'flowbite-svelte';
 	import { onMount } from 'svelte';
-	
+	import { TabulatorFull as Tabulator } from 'tabulator-tables';
+	import 'tabulator-tables/dist/css/tabulator.min.css';
+  
 	export let data;
-
-	// Might need these for tabular view
-	let innerWidth: any = null; // defining this solve the of svelte:window tag  
-	let innerHeight: any = null; // and now all other elements are visible
-	let screenWidth: any = null;
-	let screenHeight: any = null;
-
-    document.addEventListener('DOMContentLoaded', () => {
-    const createButtons = document.querySelectorAll('.create-button');
-    const tableContainer = document.getElementById('tableContainer');
-
-    // Function to create the table when any "Create New Spreadsheet" button is clicked
-    createButtons.forEach((button) => {
-        button.addEventListener('click', () => {
-            const table = new Tabulator.default(tableContainer, {
-                // Tabulator configuration options here
-                columns: [
-                    { title: 'Column 1', field: 'col1' },
-                    { title: 'Column 2', field: 'col2' },
-                    // Add more columns as needed
-                ],
-                data: [], // Initial data (can be empty)
-                // Other configuration options go here
-            });
-
-            // You can populate the table with data as needed
-            // For example:
-            // table.setData([{ col1: 'Value 1', col2: 'Value 2' }]);
-        });
-    });
-
-    // You can also add logic for the "Open New Spreadsheet" button if needed
-    // Example:
-    /*
-    const openButtons = document.querySelectorAll('.open-button');
-    openButtons.forEach((button) => {
-        button.addEventListener('click', () => {
-            // Add logic for opening a new spreadsheet here
-        });
-    });
-    */
-});
-     
-</script>
-
-<Intro heading={data.meta.title} description={data.meta.description} />
-
-<svelte:window bind:innerWidth bind:innerHeight />
-
-<section class="bg-white dark:bg-gray-800">
-    <div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
-        <div class="flex justify-center space-x-4">
-            <button id="openButton" class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-5 px-20 rounded">
-                Open New Spreadsheet (.csv)
-            </button>
-            <button id="createButton" class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-5 px-20 rounded">
-                Create New Spreadsheet (.csv)
-            </button>
-        </div>
-    </div>
-</section>
-
+	let innerWidth: any = null;
+	let innerHeight: any = null;
+  
+	let table: any = null;
+  
+	onMount(() => {
+	  // Function to initialize Tabulator
+	  const initTabulator = () => {
+		// Options for the Tabulator instance
+		const tableOptions = {
+		  columns: [],
+		  data: [], // Initial empty data
+		};
+  
+		// Initialize Tabulator
+		table = new Tabulator('#my-table', tableOptions);
+	  };
+  
+	  // Call the function to initialize Tabulator
+	  initTabulator();
+	});
+  
+	// Function to handle opening a new spreadsheet
+	const openNewSpreadsheet = () => {
+	  const input = document.createElement('input');
+	  input.type = 'file';
+  
+	  input.addEventListener('change', (event) => {
+		const file = (event.target as HTMLInputElement).files?.[0];
+  
+		if (file) {
+		  const reader = new FileReader();
+  
+		  reader.onload = (e) => {
+			const csvData = (e.target as FileReader).result;
+  
+			// Clear existing data
+			table.setData([]);
+  
+			// Parse the CSV data and set it to the table
+			Papa.parse(csvData as string, {
+			  header: true,
+			  skipEmptyLines: true,
+			  dynamicTyping: true,
+			  complete: (result) => {
+				table.setColumns(result.meta.fields);
+				table.setData(result.data);
+			  },
+			});
+		  };
+  
+		  // Read the file as text
+		  reader.readAsText(file);
+		}
+	  });
+  
+	  // Trigger the file input dialog
+	  input.click();
+	};
+  
+	// Function to handle creating a new spreadsheet
+	const createNewSpreadsheet = () => {
+	  // Clear existing data
+	  table.setData([]);
+	};
+  </script>
+  
+  <Intro heading={data.meta.title} description={data.meta.description} />
+  
+  <svelte:window bind:innerWidth bind:innerHeight />
+  
+  <section class="bg-white dark:bg-gray-900">
+	<div class="flex justify-center items-center">
+	  <Button on:click={openNewSpreadsheet} size="xl" outline color="blue" class="m-6">
+		Open New Spreadsheet<br/>(.csv)
+	  </Button>
+	  <GradientButton on:click={createNewSpreadsheet} size="xl" color="blue" class="m-6">
+		Create New Spreadsheet<br/>(.csv)
+	  </GradientButton>
+	</div>
+  
+	<!-- Table container -->
+	<div id="my-table"></div>
+  </section>
