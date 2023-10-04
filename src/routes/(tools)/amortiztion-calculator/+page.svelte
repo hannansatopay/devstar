54c68ee71de8cd60bb32ed2de58b6631b5908b16
Abhishek-2004-17 <117198301+Abhishek-2004-17@users.noreply.github.com>
2023-10-04 21:16:53 +0530

    Merge branch 'main' of https://github.com/Abhishek-2004-17/devstar

---
+++ src/routes/(tools)/amortiztion-calculator/+page.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+	import Intro from '$lib/Intro.svelte';
+  	
+	export let data;
+	
+	let start=1000;
+    let year = 15;
+    let interest = 5;
+    let yes =false;
+    let current_year =2023;
+</script>
+
+<Intro heading={data.meta.title} description={data.meta.description} />
+
+<h1>Amortization Calculator</h1>
+<form action="">
+    Loan Amount<input type="number" class="sign" bind:value={start}>
+    Loan Term<input type="number"  bind:value={year} placeholder="Year">
+    <input type="number" placeholder="month">
+    Interest rate<input type="number" bind:value={interest}>
+    <br>
+    <input type="checkbox" bind:checked={yes}> Optional: make extra
+    <br>
+    {#if yes}
+    Loan start date 
+    <select>
+        <option value="1" selected>Jan</option>
+        <option value="2">Feb</option>
+        <option value="3">Mar</option>
+        <option value="4">Apr</option>
+        <option value="5">May</option>
+        <option value="6">Jun</option>
+        <option value="7">Jul</option>
+        <option value="8">Aug</option>
+        <option value="9">Sep</option>
+        <option value="10">Oct</option>
+        <option value="11">Nov</option>
+        <option value="12">Dec</option>
+    </select> 
+    <input type="number" value={current_year}>
+    <br>
+    Extra monthly pay 
+    <br>
+    <input type="number" class="monthly_pay">
+    {/if}
+</form>
+<style>
+    h1{
+        color: blue;
+        text-align: center;
+    }
+    form{
+        padding: 4%;
+        text-align: center;
+    }
+    .monthly_pay{
+        width: 100px;
+    }
+</style>
--- src/routes/tools.js
+++ src/routes/tools.js
@@ -94,5 +94,11 @@ export let tools = {
         icon: 'spider-web',
         link: '/robots-txt-generator',
         description: "The Robots.txt Generator is a must-have tool for developers to create the robots.txt file, which controls search engine crawlers' access to specific pages or directories on a website. This tool offers a user-friendly interface to define crawling rules, including allow and disallow directives, to ensure optimal search engine indexing and prevent unwanted content from being indexed. Create and customize your robots.txt file efficiently with this generator to enhance your website's search engine optimization (SEO) efforts."
+    },
+    'amortiztion-calculator': {
+        name: 'amortiztion-calculator',
+        icon: 'spider-web',
+        link: '/amortiztion-calculator',
+        description: "There are two general definitions of amortization. The first is the systematic repayment of a loan over time. The second is used in the context of business accounting and is the act of spreading the cost of an expensive and long-lived item over many periods. The two are explained in more detail in the sections below."
     }
 };
