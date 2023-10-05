0186bc7021d721517991c516bc7d0a530fcd66aa
Tombstoner <akshansh2002@gmail.com>
2023-10-05 20:29:08 +0530

    Added frontpage for our project code-beautify

--- src/routes/(tools)/code-beautify/+page.svelte
+++ src/routes/(tools)/code-beautify/+page.svelte
@@ -1,19 +1,46 @@
-<svelte:head>
-	<title> CodeBeautify </title>
-</svelte:head>
 <script lang="ts">
-	import { onMount } from 'svelte';
-	import { Label, Input } from 'flowbite-svelte';
+	import { Label, Input, Select, Textarea } from 'flowbite-svelte';
 	import Intro from '$lib/Intro.svelte';
 	import Copy from '$lib/Copy.svelte';
+
 	export let data;
 
-	
+	let type = 'JSON';
+	let types = [
+		{ value: 'JSON', name: 'JSON Parser' },
+		{ value: 'HTML', name: 'HTML Parser' },
+		{ value: 'XML', name: 'XML Parser' }
+	];
+
+	let space = 1;
+	let string = '';
 </script>
 
 <Intro heading={data.meta.title} description={data.meta.description} />
 
+<section class="bg-white dark:bg-gray-900">
+	<div class="py-8 px-4 mx-auto max-w-screen-xl lg:px-12">
+		<div class="card gap-16 items-center mx-auto max-w-screen-xl overflow-hidden rounded-lg">
+			<div class="p-8 lg:grid lg:grid-cols-2 gap-16">
+				<Label>Select Language
+					<Select items={types} bind:value={type} />
+				</Label>
+
+				<Label>Spaces
+					<Input type="number" bind:value={space} min="1" max="10" />
+				</Label>
+			</div>
+			<div class="p-10 relative border-t-2 border-gray-500 text-gray-900 dark:text-white">
+				<Label for="textarea" class="mb-2">{type} Code</Label>
+				
+				<Textarea id="textarea" rows="20" bind:value={string} class="h-full max-h-96 resize-none"/>
+				
+				<Copy />
+				<button type="button" class="mt-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Beautify</button>
+			</div>
+		</div>
+	</div>
+</section>
 
 <style>
-	
 </style>
