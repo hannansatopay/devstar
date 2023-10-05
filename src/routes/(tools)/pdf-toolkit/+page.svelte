e2fdd0769ab5995e01447ad8a8e0c2877b0feb5d
Jyotiraditya Patil <patiljyotiraditya123@gmail.com>
2023-10-05 14:29:38 +0530

    Added Logic for FileName and Password

--- src/routes/(tools)/pdf-toolkit/+page.svelte
+++ src/routes/(tools)/pdf-toolkit/+page.svelte
@@ -2,36 +2,34 @@
   import { Label, Input, Range } from 'flowbite-svelte';
   import Intro from '$lib/Intro.svelte';
   import Copy from '$lib/Copy.svelte';
-  let file = null; 
-  let fileName = "No file chosen"; 
-  let isDecrypted = false; 
+  import { PDFDocument, rgb } from 'pdf-lib';
+
+  let file = null;
+  let fileName = "No file chosen";
+  let password = "";
+  let isDecrypted = false;
 
   const handleFile = (e) => {
     const selectedFile = e.target.files[0];
     if (selectedFile) {
       file = selectedFile;
-      fileName = selectedFile.name; 
+      fileName = selectedFile.name;
     } else {
       file = null;
       fileName = "No file chosen";
     }
+    console.log(fileName);
   }
 
-  const decryptAndDownload = () => {
-    isDecrypted = true;
-    setTimeout(() => {
-      const a = document.createElement('a');
-      const url = URL.createObjectURL(file);
-      a.href = url;
-      a.download = fileName;
-      document.body.appendChild(a);
-      a.click();
-      URL.revokeObjectURL(url);
-      document.body.removeChild(a);
-    }, 2000);
+  const handlePass = (e) => {
+    password=e.target.value;
+    console.log(password);
   }
-  export let data;
+  
+  
+export let data;
 </script>
+
 
 <Intro heading={data.meta.title} description={data.meta.description} />
 <div class="flex justify-center font-sans">
@@ -42,7 +40,7 @@
       </svg>
       <h2 class="text-xl font-medium text-gray-700 tracking-wide">Decrypt your pdf for free</h2>
       <p class="mt-2 text-gray-500 tracking-wide">Upload your pdf</p>
-      <input id="dropzone-file" type="file" class="mt-4 hidden" accept="" style="color:black" on:change={handleFile} />
+      <input id="dropzone-file" type="file" class="mt-4 hidden" accept=".pdf" style="color:black" on:change={handleFile} />
     </label>
   </section>
 </div>
@@ -50,16 +48,20 @@
   <div class="" style="color: white;">{fileName}</div>
 </div>
 <div class="flex justify-center font-sans mt-3">
-  <input type="text"  class="border rounded toggle" placeholder="Password">
+  <input type="text" class="border rounded toggle" placeholder="Password"  on:change={handlePass} >
 </div>
-
 <div class="flex justify-center mt-5">
+  <button  type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Decrypt Now</button>
+</div>
+<!-- on:click={decryptAndDownload} -->
+
+<!-- <div class="flex justify-center mt-5">
   {#if !isDecrypted}
     <button on:click={decryptAndDownload} type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Decrypt Now</button>
   {:else}
     <a href="javascript:void(0);" class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 focus:outline-none dark:focus:ring-green-800" download={fileName}>Download</a>
   {/if}
-</div>
+</div> -->
 
 <style>
 </style>
