d81d3bb513052835173efc8ae68ed3079d25371a
Abhishek-2004-17 <117198301+Abhishek-2004-17@users.noreply.github.com>
2023-10-06 19:41:47 +0530

    added amortization calculator

--- src/routes/(tools)/amortiztion-calculator/+page.svelte
+++ src/routes/(tools)/amortiztion-calculator/+page.svelte
@@ -9,57 +9,100 @@
     let yes =false;
     let current_year =2023;
     let monthly_pay =0;
+    let yearly_pay =0;
+    let one_time=0;
 </script>
 
 <Intro heading={data.meta.title} description={data.meta.description} />
 
 <h1>Amortization Calculator</h1>
-<form action="">
-    Loan Amount<input type="number" class="sign" bind:value={start}>
-    Loan Term<input type="number"  bind:value={year} placeholder="Year">
-    <input type="number" placeholder="month">
-    Interest rate<input type="number" bind:value={interest}>
-    <br>
-    <input type="checkbox" bind:checked={yes}> Optional: make extra
-    <br>
-    {#if yes}
-    Loan start date 
-    <select>
-        <option value="1" selected>Jan</option>
-        <option value="2">Feb</option>
-        <option value="3">Mar</option>
-        <option value="4">Apr</option>
-        <option value="5">May</option>
-        <option value="6">Jun</option>
-        <option value="7">Jul</option>
-        <option value="8">Aug</option>
-        <option value="9">Sep</option>
-        <option value="10">Oct</option>
-        <option value="11">Nov</option>
-        <option value="12">Dec</option>
-    </select> 
-    <input type="number" value={current_year}>
-    <br>
-    Extra monthly pay 
-    <br>
-    <input type="number" value={monthly_pay}>
-    <select>
-        <option value="1" selected>Jan</option>
-        <option value="2">Feb</option>
-        <option value="3">Mar</option>
-        <option value="4">Apr</option>
-        <option value="5">May</option>
-        <option value="6">Jun</option>
-        <option value="7">Jul</option>
-        <option value="8">Aug</option>
-        <option value="9">Sep</option>
-        <option value="10">Oct</option>
-        <option value="11">Nov</option>
-        <option value="12">Dec</option>
-    </select> 
-    <input type="number" class="monthly_pay" value={current_year}>
-    {/if}
-</form>
+    <form action="">
+        Loan Amount<input type="number" class="sign" bind:value={start}>
+        Loan Term<input type="number"  bind:value={year} placeholder="Year">
+        <input type="number" placeholder="month">
+        Interest rate<input type="number" bind:value={interest}>
+        <br>
+        <input type="checkbox" bind:checked={yes}> Optional: make extra
+        <br>
+        {#if yes}
+        Loan start date 
+        <select>
+            <option value="1" selected>Jan</option>
+            <option value="2">Feb</option>
+            <option value="3">Mar</option>
+            <option value="4">Apr</option>
+            <option value="5">May</option>
+            <option value="6">Jun</option>
+            <option value="7">Jul</option>
+            <option value="8">Aug</option>
+            <option value="9">Sep</option>
+            <option value="10">Oct</option>
+            <option value="11">Nov</option>
+            <option value="12">Dec</option>
+        </select> 
+        <input type="number" value={current_year}>
+        <br>
+        Extra monthly pay 
+        <br>
+        <input type="number" class="monthly_pay" value={monthly_pay}>
+        from 
+        <select>
+            <option value="1" selected>Jan</option>
+            <option value="2">Feb</option>
+            <option value="3">Mar</option>
+            <option value="4">Apr</option>
+            <option value="5">May</option>
+            <option value="6">Jun</option>
+            <option value="7">Jul</option>
+            <option value="8">Aug</option>
+            <option value="9">Sep</option>
+            <option value="10">Oct</option>
+            <option value="11">Nov</option>
+            <option value="12">Dec</option>
+        </select> 
+        <input type="number" class="monthly_pay" value={current_year}>
+        <br>
+        Extra yearly pay 
+        <br>
+        <input type="number" class="monthly_pay" value={yearly_pay}>
+        from 
+        <select>
+            <option value="1" selected>Jan</option>
+            <option value="2">Feb</option>
+            <option value="3">Mar</option>
+            <option value="4">Apr</option>
+            <option value="5">May</option>
+            <option value="6">Jun</option>
+            <option value="7">Jul</option>
+            <option value="8">Aug</option>
+            <option value="9">Sep</option>
+            <option value="10">Oct</option>
+            <option value="11">Nov</option>
+            <option value="12">Dec</option>
+        </select> 
+        <input type="number" class="monthly_pay" value={current_year}>
+        <br>
+        Extra one-time pay 
+        <br>
+        <input type="number" class="monthly_pay" value={one_time}>
+        in 
+        <select>
+            <option value="1" selected>Jan</option>
+            <option value="2">Feb</option>
+            <option value="3">Mar</option>
+            <option value="4">Apr</option>
+            <option value="5">May</option>
+            <option value="6">Jun</option>
+            <option value="7">Jul</option>
+            <option value="8">Aug</option>
+            <option value="9">Sep</option>
+            <option value="10">Oct</option>
+            <option value="11">Nov</option>
+            <option value="12">Dec</option>
+        </select> 
+        <input type="number" class="monthly_pay" value={current_year}>
+        {/if}
+    </form>
 <style>
     h1{
         color: blue;
