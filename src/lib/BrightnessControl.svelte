7265cea2a0fed96ed4bbbe207de064ab754d5ba4
Rajanikant Metri <50714459+coep05@users.noreply.github.com>
2023-10-08 17:06:03 +0530

    Create settings.json

---
+++ src/lib/BrightnessControl.svelte
@@ -0,0 +1,30 @@
+<!-- src/BrightnessControl.svelte -->
+<script>
+    let brightnessValue = 100; // Initial brightness value
+    let imageStyle = `brightness(${brightnessValue}%)`;
+  
+    function updateBrightness() {
+      imageStyle = `brightness(${brightnessValue}%)`;
+    }
+  </script>
+  
+  <div class="brightness-control">
+    <label for="brightness-slider">Brightness:</label>
+    <input
+      type="range"
+      id="brightness-slider"
+      min="0"
+      max="200"
+      step="1"
+      bind:value={brightnessValue}
+      on:input={updateBrightness}
+    />
+    <output>{brightnessValue}%</output>
+  </div>
+  
+  <style>
+    .brightness-control {
+      margin-top: 20px;
+    }
+  </style>
+  
