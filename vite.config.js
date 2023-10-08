27ff81204853b1f7ec960607aaf7b1e126e22c42
Sudeep-Poojary <sudeeppoojary0320@gmail.com>
2023-10-08 19:19:22 +0530

    Initial Commit

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
+import Icons from 'unplugin-icons/vite';
+
+export default defineConfig({
+	plugins: [
+		sveltekit(),
+		Icons({
+			compiler: 'svelte',
+			autoInstall: true
+		})
+	]
+});
