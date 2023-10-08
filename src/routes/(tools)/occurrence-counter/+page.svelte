a7f38e298f5a05b3d8eea1379f19b8797bc3cdbb
US30 <utkarsh.sinha@somaiya.edu>
2023-10-08 17:11:30 +0530

    Update +page.svelte
    done

--- src/routes/(tools)/occurrence-counter/+page.svelte
+++ src/routes/(tools)/occurrence-counter/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts"> 
 import Intro from '$lib/Intro.svelte';
 import jsPDF from 'jspdf';
+import { Button } from 'flowbite-svelte';
 
 	export let data;
 
@@ -24,8 +25,8 @@ import jsPDF from 'jspdf';
   doc.text(`Search String: ${searchStr}`, 20, 30);
   doc.text(`Occurrences: ${occurrences}`, 20, 40);
   doc.save('Occurrence-counter.pdf');
-}
-function copyText() {
+  }
+  function copyText() {
         if (inputText.length > 0) {
             var textarea = document.createElement("textarea");
             textarea.value = occurrences.toString();
@@ -35,6 +36,25 @@ function copyText() {
             document.body.removeChild(textarea);
         }
       }
+
+  function downloadText() {
+  if (inputText.length > 0) {
+    var filename = "DevStarPalindrome.txt";
+    var content = `Input Text: ${inputText}\nSearch String: ${searchStr}\nOccurrences: ${occurrences}`;
+
+    var blob = new Blob([content], { type: 'text/plain' });
+    var url = window.URL.createObjectURL(blob);
+
+    var a = document.createElement('a');
+    a.href = url;
+    a.download = filename;
+    document.body.appendChild(a);
+    a.click();
+
+    window.URL.revokeObjectURL(url);
+    document.body.removeChild(a);
+  }
+}
 </script>
 
 <Intro heading={data.meta.title} description={data.meta.description} />
@@ -42,34 +62,40 @@ function copyText() {
 <section class="bg-white dark:bg-gray-900">
   <div class="py-8 px-4 mx-auto max-w-screen-xl lg:px-12">
     <div class="card p-8 relative items-center mx-auto max-w-screen-xl overflow-hidden rounded-lg">
-        <div class="mt-3 gap-2 items-center mx-auto max-w-screen-xl lg:grid lg:grid-cols-1 overflow-hidden"> 
-            <div class="rounded-lg overflow-hidden bg-gray-50 border border-gray-300" id="tarea2">
-                <div class="grid grid-cols-2 gap-0.5">
+        <div class="mt-2 gap-2 items-center mx-auto max-w-screen-xl lg:grid lg:grid-cols-1 overflow-hidden"> 
+            <div class="rounded-lg overflow-hidden border border-gray-300" id="tarea2">
+                <div class="grid grid-cols-2 gap-2">
                     <div class="col-span-1"> 
                         <textarea placeholder="Search String" id="textbox" rows="1" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" style= "resize: none;"
                             bind:value={searchStr}></textarea>
                     </div>
-                    <div class="col-span-1"> 
-                        <textarea placeholder="Occurrence: 0" id="textbox" rows="1" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"style= "resize: none;"
-                        on:input={countOccurrences} bind:value={occurrences}></textarea>
-                    </div>
-                </div>
+                    <div class="col-span-1">
+                      <div class="relative">
+                        <textarea placeholder="" id="textbox" rows="1" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" style= "resize: none;"
+                        on:input={countOccurrences}></textarea>
+                          <p class="absolute inset-y-0 left-2 flex items-center text-sm text-gray-400">Occurrences: {occurrences}</p>
+                      </div>
+                  </div>
             </div>
-            <div class="rounded-lg overflow-hidden bg-gray-50 border border-gray-300" id="tarea1">
+            <div class="rounded-lg overflow-hidden bg-gray-50 border border-gray-300 mt-6" id="tarea1">
                 <textarea placeholder="Input Text" id="textbox" rows="8" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" style= "resize: none;"
                 on:input={countOccurrences} bind:value={inputText}></textarea>
             </div>
         </div>
     
         <div id="buttonArea">
-				
-        <Button color="blue" on:click={copyText}>Copy</Button>
-				<Button color="blue" on:click={downloadPDF}>Download as pdf</Button>
-			</div>
+				  <Button color="blue" on:click={copyText}>Copy</Button>
+          <Button color="blue" on:click={downloadText}>Download as txt</Button>
+				  <Button color="blue" on:click={downloadPDF}>Download as pdf</Button>
+        </div>
     </div>
+  </div>
+
+			
+    
 			
 
-	</div>
+	
 	
 	 
 </section>
