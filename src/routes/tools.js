20c3571debcb06add69fea95775eb6413a123853
Gayatri Batgeri <gayatribatgeri27@gmail.com>
2023-10-08 18:12:51 +0530

    first commit

--- src/routes/tools.js
+++ src/routes/tools.js
@@ -1,5 +1,11 @@
 export let tools = {
-<<<<<<< HEAD
+	'case-converter': {
+		name: 'Case Converter',
+		icon: 'arrow-right-arrow-left',
+		link: '/case-converter',
+		description:
+			'The Case Converter is a versatile and user-friendly tool designed to streamline the manipulation of text cases for developers, content creators, and anyone dealing with text formatting. It provides a convenient way to convert text between different letter case styles, making it easy to modify and standardize the appearance of text in various contexts. Whether you need to convert text to uppercase, lowercase, sentence case, or other formats, this tool simplifies the process, saving you time and effort.'
+	},
 	'unit-converter': {
 		name: 'Unit Converter',
 		icon: 'arrow-right-arrow-left',
@@ -14,56 +20,39 @@ export let tools = {
 		description:
 			'Create captivating palindromes effortlessly with our Palindrome Generator. Input your text and watch as it transforms into flawless symmetrical words that read the same backward and forward. Perfect for language enthusiasts, poets, and puzzle lovers. Explore different lengths, styles, and tones to enhance your linguistic creativity. Start crafting mesmerizing palindromes effortlessly and let your words resonate in perfect harmony.'
 	},
-	'case-converter': {
-		name: 'Case Converter',
+	'find-and-replace-text': {
+		name: 'Find and Replace Text',
 		icon: 'arrow-right-arrow-left',
-		link: '/case-converter',
+		link: '/find-and-replace-text',
 		description:
-			'The Case Converter is a comprehensive tool that simplifies the conversion of various Strings for developers. It can convert text to uppercase, lowercase, camel case, sentence case ,  and other styles, making it useful for formatting and editing text and code. Case converters help ensure consistency and readability in written content and simplify tasks involving text manipulation.'
+			'A find and replace text string utility tool is a feature that enables users to search for specific text within a string and replace it with another specified text. This tool is valuable for efficiently editing and updating content, making it a time-saving solution for tasks like content editing, code refactoring, and data manipulation. Users can easily locate and substitute multiple occurrences of a target string, streamlining content management and data processing workflows.'
+	},
+	'remove-space': {
+		name: 'Remove Extra Spaces',
+		icon: 'arrow-right-arrow-left',
+		link: '/remove-space',
+		description:
+			"The 'Remove Extra Spaces' button allows you to clean up your text by eliminating redundant spaces.Sometimes, when typing or copying text, extra spaces can unintentionally appear between words. Clicking the 'Remove Extra Spaces' button will streamline your text by replacing consecutive spaces with a single space, giving your text a cleaner and more uniform appearance."
+	},
+	'string-length-calculator': {
+		name: 'String Length Calculator',
+		icon: 'arrow-right-arrow-left',
+		link: '/string-length-calculator',
+		description:
+			'Discover character counts instantly! Input text into our String Length Calculator to find the total number of characters, including spaces and special characters. Ideal for writers, coders, and anyone tracking character limits. Simplify text management for any length – from short phrases to lengthy paragraphs. Take control of character counts effortlessly. Try our String Length Calculator today!'
+	},
+	'Remove-Line-Breaks': {
+		name: 'Remove Line Breaks',
+		icon: 'arrow-right-arrow-left',
+		link: '/Remove-Line-Breaks',
+		description:
+			"A line breaks removal tool is a simple utility that eliminates newline characters, such as '\n' or '\r', from a text document or string. It's useful for formatting text by joining fragmented lines into a single continuous block of text. Users can input the text with line breaks, and the tool processes it to produce a version with no line breaks, aiding in readability and further data processing."
+	},
+	'occurrence-counter': {
+		name: 'Occurrence Counter',
+		icon: 'arrow-right-arrow-left',
+		link: '/occurrence-counter',
+		description:
+			"Our word frequency counter allows you to count the frequency usage of each word in your text. Paste or type in your text below, and click submit.Discover character counts instantly!. A string occurrence counter is a utility that helps you find and tally the number of times a specific substring or character sequence appears within a given text or string. It's a valuable tool for tasks such as text analysis, search functionality, and data processing, providing quick insights into the frequency of a particular pattern within a larger text body."
 	}
 };
-=======
-    'unit-converter': {
-        name: 'Unit Converter',
-        icon: 'arrow-right-arrow-left',
-        link: '/unit-converter',
-        description: "The Unit Converter is a comprehensive tool that simplifies the conversion of various units for developers. With support for a wide range of unit types, such as length, weight, temperature, time, and more, this tool allows you to convert values seamlessly between different measurement systems. Save time and effort by integrating this versatile unit converter into your development toolkit for quick and accurate unit conversions."
-    },
-    'palindrome-generator': {
-        name: 'Palindrome Generator',
-        icon: 'arrow-right-arrow-left',
-        link: '/palindrome-generator',
-        description: "Create captivating palindromes effortlessly with our Palindrome Generator. Input your text and watch as it transforms into flawless symmetrical words that read the same backward and forward. Perfect for language enthusiasts, poets, and puzzle lovers. Explore different lengths, styles, and tones to enhance your linguistic creativity. Start crafting mesmerizing palindromes effortlessly and let your words resonate in perfect harmony."
-    },
-    'find-and-replace-text': {
-        name: 'Find and Replace Text',
-        icon: 'arrow-right-arrow-left',
-        link: '/find-and-replace-text',
-        description: "A find and replace text string utility tool is a feature that enables users to search for specific text within a string and replace it with another specified text. This tool is valuable for efficiently editing and updating content, making it a time-saving solution for tasks like content editing, code refactoring, and data manipulation. Users can easily locate and substitute multiple occurrences of a target string, streamlining content management and data processing workflows."
-    },
-    'remove-space': {
-        name: 'Remove Extra Spaces',
-        icon: 'arrow-right-arrow-left',
-        link: '/remove-space',
-        description: "The 'Remove Extra Spaces' button allows you to clean up your text by eliminating redundant spaces.Sometimes, when typing or copying text, extra spaces can unintentionally appear between words. Clicking the 'Remove Extra Spaces' button will streamline your text by replacing consecutive spaces with a single space, giving your text a cleaner and more uniform appearance."
-    },
-    'string-length-calculator': {
-        name: 'String Length Calculator',
-        icon: 'arrow-right-arrow-left',
-        link: '/string-length-calculator',
-        description: "Discover character counts instantly! Input text into our String Length Calculator to find the total number of characters, including spaces and special characters. Ideal for writers, coders, and anyone tracking character limits. Simplify text management for any length – from short phrases to lengthy paragraphs. Take control of character counts effortlessly. Try our String Length Calculator today!"
-    },
-    'Remove-Line-Breaks': {
-        name: 'Remove Line Breaks',
-        icon: 'arrow-right-arrow-left',
-        link: '/Remove-Line-Breaks',
-        description: "A line breaks removal tool is a simple utility that eliminates newline characters, such as '\n' or '\r', from a text document or string. It's useful for formatting text by joining fragmented lines into a single continuous block of text. Users can input the text with line breaks, and the tool processes it to produce a version with no line breaks, aiding in readability and further data processing."
-    },
-    'occurrence-counter': {
-        name: 'Occurrence Counter',
-        icon: 'arrow-right-arrow-left',
-        link: '/occurrence-counter',
-        description: "Our word frequency counter allows you to count the frequency usage of each word in your text. Paste or type in your text below, and click submit.Discover character counts instantly!. A string occurrence counter is a utility that helps you find and tally the number of times a specific substring or character sequence appears within a given text or string. It's a valuable tool for tasks such as text analysis, search functionality, and data processing, providing quick insights into the frequency of a particular pattern within a larger text body." 
-    },
-};
->>>>>>> 81a5cdf6033bc1d0a2fc999ebfd3dc7c78a8af85
