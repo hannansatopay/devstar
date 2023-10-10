c7803fcab19fbaa21e86d0585f3ed00b41821800
MRPrayh <80837351+MRPrayh@users.noreply.github.com>
2023-10-10 14:43:35 +0530

    Deleting useless files

--- src/routes/(tools)/online-spreadsheet/+page.svelte
+++ src/routes/(tools)/online-spreadsheet/+page.svelte
@@ -1,37 +1,67 @@
 <script lang="ts">
 	import Intro from '$lib/Intro.svelte';
 	import { Button, GradientButton } from 'flowbite-svelte';
-	import { Fileupload, Label, Helper } from 'flowbite-svelte';
-	import CsvEditor from './CsvEditor.svelte';
+	import { onMount } from 'svelte';
+	// import { Fileupload, Label, Helper } from 'flowbite-svelte';
+	// import CsvEditor from './CsvEditor.svelte';
+	// import Editabletable from './EditableTable.svelte';
+	import { Grid } from 'ag-grid-community';
+	import type { GridOptions } from 'ag-grid-community';
+	import 'ag-grid-community/styles/ag-grid.css';
+	import 'ag-grid-community/styles/ag-theme-alpine';
 
 	export let data;
-	
 
-	// Might need these for tabular view
-	let innerWidth: any = null; // defining this solve the error of svelte:window tag  
-	let innerHeight: any = null; // and now all other elements are visible
-	let screenWidth: any = null;
-	let screenHeight: any = null;
+	let grid: Grid;
 	
+	const uppercaseLetters = [];
+
+	for (let i = 65; i <= 90; i++) {
+		uppercaseLetters.push(String.fromCharCode(i));
+	}
+	for (let i = 0; i < 26; i++) {
+
+	}
+	const gridOptions: GridOptions = {
+			columnDefs: [
+				{ field: "A" },
+				{ field: "B" },
+				{ field: "C" },
+				{ field: "D" },
+				{ field: "E" },
+				{ field: "F" },
+			],
+			defaultColDef: {
+				sortable: true,
+				filter: true,
+				flex: 1,
+				resizable: true,
+				editable: true
+			},
+			rowData: [ 
+				{ A: "athlete", B: "age", C: "country", D: "year", E: "date", F: "sport"},
+				{ A: "athlete", B: "age", C: "country", D: "year", E: "date", F: "sport"},
+				{ A: "athlete", B: "age", C: "country", D: "year", E: "date", F: "sport"},
+				{ A: "athlete", B: "age", C: "country", D: "year", E: "date", F: "sport"},
+				{ A: "athlete", B: "age", C: "country", D: "year", E: "date", F: "sport"},
+				{ A: "athlete", B: "age", C: "country", D: "year", E: "date", F: "sport"},
+			]
+		}
+		
+	onMount(() => {
+		const gridEl = document.getElementById('Spreadsheet');
+		if (!gridEl) {
+			throw new Error('Grid Element not found!');
+		}
+		grid = new Grid(gridEl, gridOptions);
+	});
+
 </script>
 
 <Intro heading={data.meta.title} description={data.meta.description} />
 
-<svelte:window bind:innerWidth bind:innerHeight />
-
 <section class="bg-white dark:bg-gray-900">
 	<div class="flex justify-center items-center">
-		<Button size="xl" color="blue" class="m-6">Create New Spreadsheet</Button>
+		<div id="Spreadsheet" class="ag-theme-alpine" />
 	</div>
-
-	<h1 class="flex justify-center text-gray-500 dark:text-gray-400 mt-2 mb-6">OR</h1>
-
-	<div class="flex flex-wrap justify-center">
-		<!-- <Label for="with_helper" class="pb-2 ml-10">Upload Spreadsheet</Label>
-		<Fileupload id="with_helper" class="mb-2 ml-10 mr-10" accept=".csv"/>
-		<Helper class="ml-10 mb-10">CSV Files Only</Helper> -->
-		<CsvEditor />
-	</div>
-	
-	
 </section>
