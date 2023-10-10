f45afe460b9b54af32979a3fa5ee1f48457807b3
MRPrayh <80837351+MRPrayh@users.noreply.github.com>
2023-10-10 17:05:25 +0530

    Update +page.svelte

--- src/routes/(tools)/online-spreadsheet/+page.svelte
+++ src/routes/(tools)/online-spreadsheet/+page.svelte
@@ -22,14 +22,19 @@
 	// for (let i = 0; i < 26; i++) {
 
 	// }
+
+	// const keyValuePairs = {};
+
+	// for (let charCode = 65; charCode <= 90; charCode++) {
+	// 	const letter = String.fromCharCode(charCode);
+	// 	keyValuePairs[letter] = "\t\t"; // Assuming you want a tab character as the value
+	// }
 	const gridOptions: GridOptions = {
 			columnDefs: [
-				{ field: "Apple" },
-				{ field: "B" },
-				{ field: "C" },
-				{ field: "D" },
-				{ field: "E" },
-				{ field: "F" },
+				{ field: "A" },{ field: "B" },{ field: "C" },{ field: "D" },{ field: "E" },{ field: "F" },{ field: "G" },{ field: "H" },
+				{ field: "I" },{ field: "J" },{ field: "K" },{ field: "L" },{ field: "M" },{ field: "N" },{ field: "O" },{ field: "P" },
+				{ field: "Q" },{ field: "R" },{ field: "S" },{ field: "T" },{ field: "U" },{ field: "V" },{ field: "W" },{ field: "X" },
+				{ field: "Y" },{ field: "Z" },
 			],
 			defaultColDef: {
 				sortable: true,
@@ -38,13 +43,17 @@
 				resizable: true,
 				editable: true,
 			},
-			rowData: [ 
-				{ Apple: "athlete", B: "age", C: "country", D: "year", E: "date", F: "sport"},
-				{ Apple: "athlete", B: "age", C: "country", D: "year", E: "date", F: "sport"},
-				{ Apple: "athlete", B: "age", C: "country", D: "year", E: "date", F: "sport"},
-				{ Apple: "athlete", B: "age", C: "country", D: "year", E: "date", F: "sport"},
-				{ Apple: "athlete", B: "age", C: "country", D: "year", E: "date", F: "sport"},
-				{ Apple: "athlete", B: "age", C: "country", D: "year", E: "date", F: "sport"},
+			rowData: [ 		
+				{ A:"		", B:"		", C:"		", D:"		", E:"		", F:"		", G:"		", H:"		", I:"		", J:"		", K:"		", L:"		", M:"		", N:"		", O:"		", P:"		", Q:"		", R:"		", S:"		", T:"		", U:"		", V:"		", W:"		", X:"		", Y:"		", Z:"		",},
+				{ A:"		", B:"		", C:"		", D:"		", E:"		", F:"		", G:"		", H:"		", I:"		", J:"		", K:"		", L:"		", M:"		", N:"		", O:"		", P:"		", Q:"		", R:"		", S:"		", T:"		", U:"		", V:"		", W:"		", X:"		", Y:"		", Z:"		",},
+				{ A:"		", B:"		", C:"		", D:"		", E:"		", F:"		", G:"		", H:"		", I:"		", J:"		", K:"		", L:"		", M:"		", N:"		", O:"		", P:"		", Q:"		", R:"		", S:"		", T:"		", U:"		", V:"		", W:"		", X:"		", Y:"		", Z:"		",},
+				{ A:"		", B:"		", C:"		", D:"		", E:"		", F:"		", G:"		", H:"		", I:"		", J:"		", K:"		", L:"		", M:"		", N:"		", O:"		", P:"		", Q:"		", R:"		", S:"		", T:"		", U:"		", V:"		", W:"		", X:"		", Y:"		", Z:"		",},
+				{ A:"		", B:"		", C:"		", D:"		", E:"		", F:"		", G:"		", H:"		", I:"		", J:"		", K:"		", L:"		", M:"		", N:"		", O:"		", P:"		", Q:"		", R:"		", S:"		", T:"		", U:"		", V:"		", W:"		", X:"		", Y:"		", Z:"		",},
+				{ A:"		", B:"		", C:"		", D:"		", E:"		", F:"		", G:"		", H:"		", I:"		", J:"		", K:"		", L:"		", M:"		", N:"		", O:"		", P:"		", Q:"		", R:"		", S:"		", T:"		", U:"		", V:"		", W:"		", X:"		", Y:"		", Z:"		",},
+				{ A:"		", B:"		", C:"		", D:"		", E:"		", F:"		", G:"		", H:"		", I:"		", J:"		", K:"		", L:"		", M:"		", N:"		", O:"		", P:"		", Q:"		", R:"		", S:"		", T:"		", U:"		", V:"		", W:"		", X:"		", Y:"		", Z:"		",},
+				{ A:"		", B:"		", C:"		", D:"		", E:"		", F:"		", G:"		", H:"		", I:"		", J:"		", K:"		", L:"		", M:"		", N:"		", O:"		", P:"		", Q:"		", R:"		", S:"		", T:"		", U:"		", V:"		", W:"		", X:"		", Y:"		", Z:"		",},
+				
+
 			]
 		}
 		
@@ -61,13 +70,17 @@
 <Intro heading={data.meta.title} description={data.meta.description} />
 
 <section class="bg-white dark:bg-gray-900">
-	<div id="Spreadsheet" style="display:flex width: 80%; height:500px" class="ag-theme-alpine ag-theme-acmecorp"/>
+	
+	<div id="Spreadsheet" style="width:100% height:80%" class="ag-theme-alpine ag-theme-acmecorp"/>
+	
 </section>
 
 <style>
 	/* set the background color of many elements across the grid */
 .ag-theme-alpine {
-    --ag-background-color: bg-gray-900;
+    --ag-background-color-dark: bg-gray-900;
+	--ag-background-color: bg-white
+	
 }
 /* change the font style of a single UI component */
 .ag-theme-alpine .ag-header-cell-label {
