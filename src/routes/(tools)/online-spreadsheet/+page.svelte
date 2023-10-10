8403e2b9c3cb8a094059876d34dc2456360b01e1
MRPrayh <80837351+MRPrayh@users.noreply.github.com>
2023-10-10 15:37:56 +0530

    Update +page.svelte

--- src/routes/(tools)/online-spreadsheet/+page.svelte
+++ src/routes/(tools)/online-spreadsheet/+page.svelte
@@ -8,23 +8,23 @@
 	import { Grid } from 'ag-grid-community';
 	import type { GridOptions } from 'ag-grid-community';
 	import 'ag-grid-community/styles/ag-grid.css';
-	import 'ag-grid-community/styles/ag-theme-alpine';
+	import "ag-grid-community/styles/ag-theme-alpine.css"
 
 	export let data;
 
 	let grid: Grid;
 	
-	const uppercaseLetters = [];
+	// const uppercaseLetters = [];
 
-	for (let i = 65; i <= 90; i++) {
-		uppercaseLetters.push(String.fromCharCode(i));
-	}
-	for (let i = 0; i < 26; i++) {
+	// for (let i = 65; i <= 90; i++) {
+	// 	uppercaseLetters.push(String.fromCharCode(i));
+	// }
+	// for (let i = 0; i < 26; i++) {
 
-	}
+	// }
 	const gridOptions: GridOptions = {
 			columnDefs: [
-				{ field: "A" },
+				{ field: "Apple" },
 				{ field: "B" },
 				{ field: "C" },
 				{ field: "D" },
@@ -36,15 +36,15 @@
 				filter: true,
 				flex: 1,
 				resizable: true,
-				editable: true
+				editable: true,
 			},
 			rowData: [ 
-				{ A: "athlete", B: "age", C: "country", D: "year", E: "date", F: "sport"},
-				{ A: "athlete", B: "age", C: "country", D: "year", E: "date", F: "sport"},
-				{ A: "athlete", B: "age", C: "country", D: "year", E: "date", F: "sport"},
-				{ A: "athlete", B: "age", C: "country", D: "year", E: "date", F: "sport"},
-				{ A: "athlete", B: "age", C: "country", D: "year", E: "date", F: "sport"},
-				{ A: "athlete", B: "age", C: "country", D: "year", E: "date", F: "sport"},
+				{ Apple: "athlete", B: "age", C: "country", D: "year", E: "date", F: "sport"},
+				{ Apple: "athlete", B: "age", C: "country", D: "year", E: "date", F: "sport"},
+				{ Apple: "athlete", B: "age", C: "country", D: "year", E: "date", F: "sport"},
+				{ Apple: "athlete", B: "age", C: "country", D: "year", E: "date", F: "sport"},
+				{ Apple: "athlete", B: "age", C: "country", D: "year", E: "date", F: "sport"},
+				{ Apple: "athlete", B: "age", C: "country", D: "year", E: "date", F: "sport"},
 			]
 		}
 		
@@ -61,7 +61,21 @@
 <Intro heading={data.meta.title} description={data.meta.description} />
 
 <section class="bg-white dark:bg-gray-900">
-	<div class="flex justify-center items-center">
-		<div id="Spreadsheet" class="ag-theme-alpine" />
-	</div>
+	<div id="Spreadsheet" style="display:flex width: 80%; height:500px" class="ag-theme-alpine ag-theme-acmecorp"/>
 </section>
+
+<style>
+	/* set the background color of many elements across the grid */
+.ag-theme-alpine {
+    --ag-background-color: bg-gray-900;
+}
+/* change the font style of a single UI component */
+.ag-theme-alpine .ag-header-cell-label {
+    font-style: italic;
+}
+
+/* ag-theme-acmecorp.css */
+.ag-theme-acmecorp {
+    --ag-odd-row-background-color: bg-gray-900;
+}
+</style>
