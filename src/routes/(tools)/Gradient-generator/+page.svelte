2a58eea316f25893ff8ff8c381e75e9dade42d93
Yash Mankar <82888014+AraeneaCLI@users.noreply.github.com>
2023-10-10 11:56:01 +0530

    Update +page.svelte

--- src/routes/(tools)/Gradient-generator/+page.svelte
+++ src/routes/(tools)/Gradient-generator/+page.svelte
@@ -36,16 +36,6 @@
    function updateGradient() {
     generateGradient();
   }
-
-  function getRandomColor() {
-    const letters = "0123456789ABCDEF";
-    let color = "#";
-    for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    gradientCode = color;
-  }
-
 </script>
 
 <Intro heading={data.meta.title} description={data.meta.description}/>
