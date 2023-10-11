12ed97999457efab24064cb79a4f5caf13ebdec9
Abdur Rehman Shaikh <abdurrahman19702@gmail.com>
2023-10-11 21:33:20 +0530

    Added Binary Text Converter and removed Icons

--- src/routes/tools.js
+++ src/routes/tools.js
@@ -2,7 +2,6 @@ export let tools = {
 	
 	'unit-converter': {
 		name: 'Unit Converter',
-		icon: 'arrow-right-arrow-left',
 		link: '/unit-converter',
 		description:
 		'The Unit Converter is a comprehensive tool that simplifies the conversion of various units for developers. With support for a wide range of unit types, such as length, weight, temperature, time, and more, this tool allows you to convert values seamlessly between different measurement systems. Save time and effort by integrating this versatile unit converter into your development toolkit for quick and accurate unit conversions.'
@@ -10,7 +9,6 @@ export let tools = {
 
 	'palindrome-generator': {
 		name: 'Palindrome Generator',
-		icon: 'arrow-right-arrow-left',
 		link: '/palindrome-generator',
 		description:
 		'Create captivating palindromes effortlessly with our Palindrome Generator. Input your text and watch as it transforms into flawless symmetrical words that read the same backward and forward. Perfect for language enthusiasts, poets, and puzzle lovers. Explore different lengths, styles, and tones to enhance your linguistic creativity. Start crafting mesmerizing palindromes effortlessly and let your words resonate in perfect harmony.'
@@ -18,7 +16,6 @@ export let tools = {
 
     'case-converter': {
 		name: 'Case Converter',
-		icon: 'arrow-right-arrow-left',
 		link: '/case-converter',
 		description:
 		'The Case Converter is a comprehensive tool that simplifies the conversion of various Strings for developers. It can convert text to uppercase, lowercase, camel case, sentence case ,  and other styles, making it useful for formatting and editing text and code. Case converters help ensure consistency and readability in written content and simplify tasks involving text manipulation.'
@@ -26,7 +23,6 @@ export let tools = {
 
     'remove-duplicate-text': {
 		name: 'Remove Duplicate Text',
-		icon: 'arrow-right-arrow-left',
 		link: '/remove-duplicate-text',
 		description:
 		'Remove Duplicate Lines and Words Tool is the ultimate solution for swiftly eliminating redundancy in your text. Whether you are a writer, coder, or data analyst, this user-friendly tool instantly identifies and cleans up repeated lines and words, saving you valuable time and ensuring your content is concise and error-free. Simplify your workflow and boost productivity with this essential text optimization tool.'
@@ -34,7 +30,6 @@ export let tools = {
 
 	'find-and-replace-text': {
 		name: 'Find and Replace Text',
-		icon: 'arrow-right-arrow-left',
 		link: '/find-and-replace-text',
 		description:
 		'A find and replace text string utility tool is a feature that enables users to search for specific text within a string and replace it with another specified text. This tool is valuable for efficiently editing and updating content, making it a time-saving solution for tasks like content editing, code refactoring, and data manipulation. Users can easily locate and substitute multiple occurrences of a target string, streamlining content management and data processing workflows.'
@@ -42,7 +37,6 @@ export let tools = {
 
 	'remove-space': {
 		name: 'Remove Extra Spaces',
-		icon: 'arrow-right-arrow-left',
 		link: '/remove-space',
 		description:
 		"The 'Remove Extra Spaces' button allows you to clean up your text by eliminating redundant spaces.Sometimes, when typing or copying text, extra spaces can unintentionally appear between words. Clicking the 'Remove Extra Spaces' button will streamline your text by replacing consecutive spaces with a single space, giving your text a cleaner and more uniform appearance."
@@ -50,7 +44,6 @@ export let tools = {
 
 	'string-length-calculator': {
 		name: 'String Length Calculator',
-		icon: 'arrow-right-arrow-left',
 		link: '/string-length-calculator',
 		description:
 		'Discover character counts instantly! Input text into our String Length Calculator to find the total number of characters, including spaces and special characters. Ideal for writers, coders, and anyone tracking character limits. Simplify text management for any length – from short phrases to lengthy paragraphs. Take control of character counts effortlessly. Try our String Length Calculator today!'
@@ -58,7 +51,6 @@ export let tools = {
 
 	'occurrence-counter': {
 		name: 'Occurrence Counter',
-		icon: 'arrow-right-arrow-left',
 		link: '/occurrence-counter',
 		description:
 		"Our word frequency counter allows you to count the frequency usage of each word in your text. Paste or type in your text below, and click submit.Discover character counts instantly!. A string occurrence counter is a utility that helps you find and tally the number of times a specific substring or character sequence appears within a given text or string. It's a valuable tool for tasks such as text analysis, search functionality, and data processing, providing quick insights into the frequency of a particular pattern within a larger text body."
@@ -66,7 +58,6 @@ export let tools = {
 
 	'upside-down-text': {
 		name: 'Upside Down Text Generator',
-		icon: 'arrow-right-arrow-left',
 		link: '/upside-down-text',
 		description:
 		"Our word frequency counter allows you to count the frequency usage of each word in your text. Paste or type in your text below, and click submit.Discover character counts instantly!. A string occurrence counter is a utility that helps you find and tally the number of times a specific substring or character sequence appears within a given text or string. It's a valuable tool for tasks such as text analysis, search functionality, and data processing, providing quick insights into the frequency of a particular pattern within a larger text body."
@@ -74,7 +65,6 @@ export let tools = {
 
     'remove-line-breaks': {
         name: 'Remove Line Breaks',
-        icon: 'arrow-right-arrow-left',
         link: '/remove-line-breaks',
         description: 
 		"A line breaks removal tool is a simple utility that eliminates newline characters, such as '\n' or '\r', from a text document or string. It's useful for formatting text by joining fragmented lines into a single continuous block of text. Users can input the text with line breaks, and the tool processes it to produce a version with no line breaks, aiding in readability and further data processing."
@@ -82,7 +72,6 @@ export let tools = {
 
     'base64-encode-decode': {
         name: 'Base64 Encoder & Decoder',
-        icon: 'arrow-right-arrow-left',
         link: '/base64-encode-decode',
         description: 
         "Experience seamless data transformation with our Base64 Encoder and Decoder tool. Whether you need to encode sensitive information or decode encoded data, our user-friendly interface simplifies the process. Input your text or encoded string, and our tool swiftly converts it to Base64 format or decodes it back to its original form. Perfect for developers, security professionals, and anyone dealing with data manipulation tasks. Explore the efficiency of Base64 encoding and decoding effortlessly, ensuring secure communication and efficient data handling. Start encoding and decoding with ease, empowering your digital endeavors."
@@ -90,7 +79,6 @@ export let tools = {
 
 	'escape-unescape-text': {
         name: 'Escape & Unescape Text',
-        icon: 'arrow-right-arrow-left',
         link: '/escape-unescape-text',
         description: 
 		"Elevate your text manipulation tasks with our Escape and Unescape Text Tool. Whether you need to escape special characters for safe transmission or unescape encoded strings, our intuitive tool simplifies the process. Input your text, and watch as it seamlessly escapes special characters or reverses the process, restoring your text to its original form. Ideal for programmers, web developers, and content creators dealing with HTML, URLs, or JavaScript strings. Enhance your workflow by effortlessly handling special characters and encoded text. Start escaping and unescaping with ease, ensuring accurate data representation in any context."	
@@ -98,7 +86,6 @@ export let tools = {
 
 	'strip-html-tags': {
         name: 'Strip HTML Tags',
-        icon: 'arrow-right-arrow-left',
         link: '/strip-html-tags',
         description: 
 		"Refine your content effortlessly with our Strip HTML Tags Tool. Input any text containing HTML markup, and watch as it instantly removes all HTML tags, leaving you with clean, readable text. Perfect for writers, editors, and developers dealing with web content, ensuring seamless integration across various platforms. Say goodbye to cluttered code and hello to polished, tag-free content. Streamline your text processing tasks, enhancing readability and user experience. Start stripping HTML tags with ease, transforming your content into its purest, most accessible form."
@@ -106,7 +93,6 @@ export let tools = {
 
 	'remove-accents': {
         name: 'Remove Accents',
-        icon: 'arrow-right-arrow-left',
         link: '/remove-accents',
         description: 
 		"Enhance the clarity of your text with our Remove Accent from Text Tool. Input any text containing accented characters, and witness as it effortlessly strips away accents, leaving behind clean and simplified text. Ideal for language enthusiasts, writers, and researchers striving for precise, accent-free communication. Ensure uniformity and readability in your documents, web content, and communications by effortlessly removing diacritical marks. Streamline your text processing tasks, making your content universally understandable. Start removing accents with ease, elevating the professionalism and accessibility of your text."
@@ -114,7 +100,6 @@ export let tools = {
 
 	'random-string-generator': {
         name: 'Random String Generator',
-        icon: 'arrow-right-arrow-left',
         link: '/random-string-generator',
         description: 
 		"Elevate your data generation tasks with our Random String Generator. Tailor your random strings based on your preferences, specifying maximum and minimum lengths, desired number of strings, and specific patterns. Whether you need diverse passwords, unique identifiers, or customized test data, our tool delivers. Perfect for developers, testers, and data analysts, enabling the creation of random strings for various applications. Craft strings of specific lengths, incorporate patterns, or generate diverse strings in bulk effortlessly. Enhance your data generation experience, ensuring seamless integration into your projects. Start generating tailored random strings with ease, empowering your data-driven endeavors."
@@ -122,7 +107,6 @@ export let tools = {
 
 	'text-reversal':{
         name: 'Text Reversal',
-        icon: 'arrow-right-arrow-left',
         link: '/text-reversal',
         description: 
 		"Experience the art of text transformation with our Text Reversal Tool. Effortlessly reverse your text, both word-wise and character-wise, with a simple input. Perfect for writers, language enthusiasts, and puzzle solvers, our tool reverses your sentences, preserving word order while also flipping individual characters. Whether you want to decode encrypted messages, play with linguistic patterns, or add a unique twist to your writing, our tool is your solution. Seamlessly switch between reversing entire sentences or just the characters within each word. Unlock the creative potential of your text, exploring reverse language constructs in both words and characters. Start exploring the world of text reversal with ease, adding depth and creativity to your linguistic endeavors."
@@ -130,7 +114,6 @@ export let tools = {
 
 	'text-truncator':{
         name: 'Text Truncator',
-        icon: 'arrow-right-arrow-left',
         link: '/text-truncator',
         description:
 		"Refine your text effortlessly with our Text Truncator Tool. Tailor your text to specific lengths, ensuring concise and polished communication. Input your text, set the desired character limit, and watch as it trims down your content while preserving its essence. Ideal for writers, marketers, and social media enthusiasts striving for impactful, concise messaging. Whether you're crafting tweets, headlines, or product descriptions, our tool helps you maintain clarity within space constraints. Simplify lengthy texts without losing their meaning, optimizing readability and engagement. Start truncating text with precision, transforming your content into concise, impactful messages that resonate with your audience." 
@@ -138,10 +121,16 @@ export let tools = {
 
 	'url-encode-decode': {
         name: 'URL Encoder & Decoder',
-        icon: 'arrow-right-arrow-left',
         link: '/url-encode-decode',
         description: 
 		"Optimize your web interactions with our URL Encoder and Decoder Tool. Encode sensitive data for secure transmission or decode encoded URLs effortlessly, ensuring seamless communication online. Input your text, and our tool swiftly converts special characters into URL-friendly formats or decodes URLs back to their original state. Ideal for developers, marketers, and anyone managing web content, our tool simplifies the handling of URLs, query strings, and form data. Enhance your website's functionality, ensuring accurate data exchange and smooth user experiences. Start encoding and decoding URLs with ease, streamlining your web interactions and data management tasks."
+    },
+
+	'binary-text-converter': {
+        name: 'Binary Text Converter',
+        link: '/binary-text-converter',
+        description:
+		"Transform text into a digital language or decode binary data with our Text to Binary and Binary to Text Converter Tool. Input your text, and instantly convert it into binary code, allowing seamless integration into digital systems or secure data transmission. Conversely, input binary digits, and watch them transform back into human-readable text. Ideal for programmers, cybersecurity professionals, and tech enthusiasts, our tool simplifies data conversion tasks. Whether you're working with computer protocols, coding messages, or decoding binary files, our converter ensures accuracy and efficiency. Start converting text to binary and vice versa with ease, bridging the gap between human language and digital communication." 
     },
 
 };
