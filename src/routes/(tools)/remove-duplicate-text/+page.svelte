c22892e0e5218a1772fac00aeed411bfd00b0c93
Abdur Rehman Shaikh <abdurrahman19702@gmail.com>
2023-10-12 23:02:42 +0530

    Updated Remove Duplicate Text

--- src/routes/(tools)/remove-duplicate-text/+page.svelte
+++ src/routes/(tools)/remove-duplicate-text/+page.svelte
@@ -1,96 +1,94 @@
-
 <script lang="ts">
 	
 	import jsPDF from 'jspdf';
 	import Intro from '$lib/Intro.svelte';
-	import { Textarea,Button } from 'flowbite-svelte';
-    export const removeDuplicateOptions = [
-	'Remove Duplicate Words',
-	'Remove Duplicate Lines',
-  ]
-
+	import { Button } from 'flowbite-svelte';
 
 	export let data;
-	let inputValue = "";
-	let outputValue = "";
+
+    export const removeDuplicateOptions = [
+		'Remove Duplicate Words',
+		'Remove Duplicate Lines',
+	]
+
+	let input = "";
+	let output = "";
 	let selectedValue = '';
 
 	function handleChange(event: Event) {
-    const selectElement = event.target as HTMLSelectElement;
-    selectedValue = selectElement.value;
-	handleOptionChange(selectedValue);
-  }
-  const inputElement = document.querySelector("#input-textarea-id");
-  if (inputElement) {
-    inputElement.addEventListener("input", handleInput);
-  }
-	function handleInput(event: Event) {
-  const inputElement = document.querySelector<HTMLInputElement>("#input-textarea-id");
-  if (inputElement) {  
-    inputValue = inputElement.value;
-  }
-  handleOptionChange((document.querySelector<HTMLSelectElement>("select") as HTMLSelectElement).value);
-}
-
-
-function handleOptionChange(selectedOption: string) {
-	  switch (selectedOption) {
-
-		case "Remove Duplicate Words":
-        const sentences = inputValue.split('.'); // Split input text into sentences
-      const cleanedSentences = sentences.map(sentence => {
-        const words = sentence.split(' '); // Split sentence into words
-        const uniqueWords = Array.from(new Set(words)); // Remove duplicate words
-        return uniqueWords.join(' '); // Join unique words back into sentence
-      });
-      outputValue = cleanedSentences.join('.');
-		  break;
-
-
-		case "Remove Duplicate Lines":
-		  const lines = inputValue.split('\n'); // Split input text into lines
-    const uniqueLines = Array.from(new Set(lines)); // Remove duplicates
-    outputValue = uniqueLines.join('\n');
-
-
-	  }
+		const selectElement = event.target as HTMLSelectElement;
+		selectedValue = selectElement.value;
+		handleOptionChange(selectedValue);
 	}
-    
-	function downloadAsText() {
-  const textarea = document.querySelector<HTMLTextAreaElement>("#output-textarea-id");
-  const text = textarea?.value;
-
-  if (typeof text === "string") {
-    const blob = new Blob([text], { type: "text/plain" });
-
-    const url = URL.createObjectURL(blob);
-
-    const a = document.createElement("a");
-    a.href = url;
-    a.download = "output.txt";
-    a.style.display = "none";
-    document.body.appendChild(a);
-    a.click();
-    document.body.removeChild(a);
-
-    URL.revokeObjectURL(url);
-  }
-}
-
-function downloadPDF() {
-		const doc = new jsPDF();
-		doc.text(outputValue, 20, 20);
-		doc.save('DevStarRemoveDuplicate.pdf');
-	}
-
-    
+  	
+	const inputElement = document.querySelector("#input-textarea-id");
   
-	function copyToClipboard() {
-	  const textarea = document.querySelector("#output-textarea-id");
-	  const textareaElement = textarea as HTMLTextAreaElement;
-textareaElement.select();
+	if (inputElement) {
+		inputElement.addEventListener("input", handleInput);
+	}
 
-	  document.execCommand("copy");
+	function handleInput(event: Event) {
+		const inputElement = document.querySelector<HTMLInputElement>("#input-textarea-id");
+		if (inputElement) {  
+			input = inputElement.value;
+		}
+	handleOptionChange((document.querySelector<HTMLSelectElement>("select") as HTMLSelectElement).value);
+	}
+
+
+	function handleOptionChange(selectedOption: string) {
+		switch (selectedOption) {
+
+			case "Remove Duplicate Words":
+			const sentences = input.split('.'); // Split input text into sentences
+				const cleanedSentences = sentences.map(sentence => {
+				const words = sentence.split(' '); // Split sentence into words
+				const uniqueWords = Array.from(new Set(words)); // Remove duplicate words
+				return uniqueWords.join(' '); // Join unique words back into sentence
+			});
+
+			output = cleanedSentences.join('.');
+			break;
+
+			case "Remove Duplicate Lines":
+			const lines = input.split('\n'); // Split input text into lines
+			const uniqueLines = Array.from(new Set(lines)); // Remove duplicates
+			output = uniqueLines.join('\n');
+		}
+	}
+    
+	function copyText() {
+		if (output.length > 0) {
+			var textarea = document.createElement("textarea");
+			textarea.value = output;
+			document.body.appendChild(textarea);
+			textarea.select();
+			document.execCommand("copy");
+			document.body.removeChild(textarea);
+		}
+	}
+
+	function downloadText() {
+		if (output.length > 0) {
+			var filename = "DevStarPalindrome.txt";
+			var blob = new Blob([output], { type: 'text/plain' });
+			var url = window.URL.createObjectURL(blob);
+			
+			var a = document.createElement('a');
+			a.href = url;
+			a.download = filename;
+			document.body.appendChild(a);
+			a.click();
+			
+			window.URL.revokeObjectURL(url);
+			document.body.removeChild(a);
+		}
+	}
+  
+	function downloadPDF() {
+		const doc = new jsPDF();
+		doc.text(output, 20, 20);
+		doc.save('DevStarPalindrome.pdf');
 	}
 
     	
@@ -101,73 +99,66 @@ textareaElement.select();
 <section class="bg-white dark:bg-gray-900">
 	<div class="py-8 px-4 mx-auto max-w-screen-xl lg:px-12">
 		<div class="card p-8 relative items-center mx-auto max-w-screen-xl overflow-hidden rounded-lg" id="box">
-			<select on:change={handleChange} 
-				class="rounded-lg bg-gray-50 text-gray-900 text-sm border-0 border-t-2 border-white focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-				<option>--Select Option-- </option>
-				{#each removeDuplicateOptions as option}
-				<option value={option}>{option}</option>
-			{/each}
-		</select>	
-			<div class="mt-3 gap-2 items-center mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 overflow-hidden">
+
+			<div class="rounded-lg overflow-hidden bg-gray-50 border border-gray-300" id="tarea1">
+				<select on:change={handleChange} 
+					class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+					<option>Select Option</option>
+					{#each removeDuplicateOptions as option}
+					<option value={option}>{option}</option>
+					{/each}
+				</select>	
+			</div>
+						
+			<div class="mt-3 gap-2 items-center mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 overflow-hidden" id="textbox">
 				
 				<div class="rounded-lg overflow-hidden bg-gray-50 border border-gray-300" id="tarea1">
-					<Textarea placeholder="Enter Text" id="input-textarea-id" rows="8" name="message"
+					<textarea placeholder="Enter Text" id="input-textarea-id" rows="8" name="message" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 					on:input={handleInput}/>
-					
 				</div>
 
 				<div class="rounded-lg overflow-hidden bg-gray-50 border border-gray-300" id="tarea2">
-					
-				<Textarea
-					placeholder="Result ...."
-					readOnly
-					id="output-textarea-id"
-					
-					rows="8"
-					
-					style="border-radius:0"
-					bind:value={outputValue}
-					/>
+					<textarea readOnly rows="8" placeholder="Result" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+					bind:value={output}/>
 				</div>
 
 			</div>
 
-			<div class="buttons" >
-				<Button color="blue" on:click={copyToClipboard}>Copy</Button>
-				<Button color="blue" on:click={downloadAsText}>Download as txt</Button>
+			<div id="buttonArea">
+				<Button color="blue" on:click={copyText}>Copy</Button>
+				<Button color="blue" on:click={downloadText}>Download as txt</Button>
 				<Button color="blue" on:click={downloadPDF}>Download as pdf</Button>
-			
-			  </div>
+			</div>
 
 		</div>
 	</div>
 </section>
 
 <style>
-	
-	
-	#box{
-		border:2px solid gray ;
-		padding:35px;
-	}
-	
-	#tarea1{
-		margin-right: 10px;
+
+	#textbox > div > textarea{
+		resize: none;
 	}
 
-	#tarea2{
-		margin-left: 10px;
+	#textbox{
+		margin-top: 20px;
+		gap:20px;
 	}
-	.buttons {
+
+	#buttonArea {
 		margin-top: 30px;
 		display: flex;
 		justify-content: center;
 		align-items: center;
-		gap: 30px;
-
+		gap: 30px; /* Adjust the gap between buttons as needed */
 	}
+
+	.card {
+		box-shadow: rgba(0, 0, 0, 0.1) 0 0 0 2px;
+	}
+
+	:is(.dark .card) {
+		box-shadow: rgba(255, 255, 255, 0.5) 0 0 0 2px;
+	}
+
 </style>
-
-
-
-
