4388d0a076344e2ebb2e40bf655e12ec592d2eed
Abdur Rehman Shaikh <abdurrahman19702@gmail.com>
2023-10-12 22:21:53 +0530

    Updated Case Converter

--- src/routes/(tools)/case-converter/+page.svelte
+++ src/routes/(tools)/case-converter/+page.svelte
@@ -1,125 +1,129 @@
 <script lang="ts">
 	
+	import { Button } from 'flowbite-svelte';
 	import jsPDF from 'jspdf';
 	import Intro from '$lib/Intro.svelte';
-	import { Textarea,Button } from 'flowbite-svelte';
-export const caseConversionOptions = [
-	'Text Case Inversion',
-	'Sentence Case',
-	'Lower Case',
-	'Upper Case',
-	'Capitalize Case',
-	'De-Capitalize Case',
-	'Snake Case',
-	'camelCase',
-	'kebab-Case'
-];
 
 	export let data;
-	let inputValue = "";
-	let outputValue = "";
+	
+	export const caseConversionOptions = [
+		'Text Case Inversion',
+		'Sentence Case',
+		'Lower Case',
+		'Upper Case',
+		'Capitalize Case',
+		'De-Capitalize Case',
+		'Snake Case',
+		'Camel Case',
+		'Kebab Case'
+	];
+	
+	let input = "";
+	let output = "";
 	let selectedValue = '';
 
 	function handleChange(event: Event) {
-    const selectElement = event.target as HTMLSelectElement;
-    selectedValue = selectElement.value;
-	handleOptionChange(selectedValue);
-  }
-  const inputElement = document.querySelector("#input-textarea-id");
-  if (inputElement) {
-    inputElement.addEventListener("input", handleInput);
-  }
+		const selectElement = event.target as HTMLSelectElement;
+		selectedValue = selectElement.value;
+		handleOptionChange(selectedValue);
+	}
+
+	const inputElement = document.querySelector("#input-textarea-id");
+	
+	if (inputElement) {
+		inputElement.addEventListener("input", handleInput);
+	}
+
 	function handleInput(event: Event) {
-  const inputElement = document.querySelector<HTMLInputElement>("#input-textarea-id");
-  if (inputElement) {
-    inputValue = inputElement.value;
-  }
-  handleOptionChange((document.querySelector<HTMLSelectElement>("select") as HTMLSelectElement).value);
-}
-function handleOptionChange(selectedOption: string) {
-	  switch (selectedOption) {
-		case "Text Case Inversion":
-		  outputValue = inputValue
-			.split("")
-			.map((char) =>
-			  char === char.toLowerCase()
-				? char.toUpperCase()
-				: char.toLowerCase()
-			)
-			.join("");
-		  break;
-		case "Sentence Case":
-		  outputValue = inputValue.charAt(0).toUpperCase() + inputValue.slice(1);
-		  break;
-		case "Upper Case":
-		  outputValue = inputValue.toUpperCase();
-		  break;
-		case "Lower Case":
-		  outputValue = inputValue.toLowerCase();
-		  break;
-		case "Capitalize Case":
-		  outputValue = inputValue
-			.split(/[\s\n]+/)
-			.map(
-			  (word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
-			)
-			.join(" ");
-		  break;
-		case "De-Capitalize Case":
-		  outputValue = inputValue
-			.split(/[\s\n]+/)
-			.map((word) => word.charAt(0).toLowerCase() + word.slice(1))
-			.join(" ");
-		  break;
-		case "Snake Case":
-		  outputValue = inputValue?.toLowerCase().replace(/\s+/g, "_");
-		  break;
-		case "camelCase":
-		  outputValue = inputValue
-			?.toLowerCase()
-			.replace(/[^a-zA-Z0-9]+(.)/g, (_, chr) => chr.toUpperCase());
-		  break;
-		case "kebab-Case":
-		  outputValue = inputValue?.toLowerCase().replace(/\s+/g, "-");
-	  }
+		const inputElement = document.querySelector<HTMLInputElement>("#input-textarea-id");
+		if (inputElement) {
+			input = inputElement.value;
+		}
+
+		handleOptionChange((document.querySelector<HTMLSelectElement>("select") as HTMLSelectElement).value);
+	}
+
+	function handleOptionChange(selectedOption: string) {
+		switch (selectedOption) {
+			case "Text Case Inversion":
+			output = input
+				.split("")
+				.map((char) =>
+				char === char.toLowerCase()
+					? char.toUpperCase()
+					: char.toLowerCase()
+				)
+				.join("");
+			break;
+			case "Sentence Case":
+			output = input.charAt(0).toUpperCase() + input.slice(1);
+			break;
+			case "Upper Case":
+			output = input.toUpperCase();
+			break;
+			case "Lower Case":
+			output = input.toLowerCase();
+			break;
+			case "Capitalize Case":
+			output = input
+				.split(/[\s\n]+/)
+				.map(
+				(word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+				)
+				.join(" ");
+			break;
+			case "De-Capitalize Case":
+			output = input
+				.split(/[\s\n]+/)
+				.map((word) => word.charAt(0).toLowerCase() + word.slice(1))
+				.join(" ");
+			break;
+			case "Snake Case":
+			output = input?.toLowerCase().replace(/\s+/g, "_");
+			break;
+			case "Camel Case":
+			output = input
+				?.toLowerCase()
+				.replace(/[^a-zA-Z0-9]+(.)/g, (_, chr) => chr.toUpperCase());
+			break;
+			case "Kebab Case":
+			output = input?.toLowerCase().replace(/\s+/g, "-");
+		}
 	}
   
-	function downloadAsText() {
-  const textarea = document.querySelector<HTMLTextAreaElement>("#output-textarea-id");
-  const text = textarea?.value;
+	function copyText() {
+		if (output.length > 0) {
+			var textarea = document.createElement("textarea");
+			textarea.value = output;
+			document.body.appendChild(textarea);
+			textarea.select();
+			document.execCommand("copy");
+			document.body.removeChild(textarea);
+		}
+	}
 
-  if (typeof text === "string") {
-    const blob = new Blob([text], { type: "text/plain" });
-
-    const url = URL.createObjectURL(blob);
-
-    const a = document.createElement("a");
-    a.href = url;
-    a.download = "output.txt";
-    a.style.display = "none";
-    document.body.appendChild(a);
-    a.click();
-    document.body.removeChild(a);
-
-    URL.revokeObjectURL(url);
-  }
-}
-
-function downloadPDF() {
+	function downloadText() {
+		if (output.length > 0) {
+			var filename = "DevStarPalindrome.txt";
+			var blob = new Blob([output], { type: 'text/plain' });
+			var url = window.URL.createObjectURL(blob);
+			
+			var a = document.createElement('a');
+			a.href = url;
+			a.download = filename;
+			document.body.appendChild(a);
+			a.click();
+			
+			window.URL.revokeObjectURL(url);
+			document.body.removeChild(a);
+		}
+	}
+  
+	function downloadPDF() {
 		const doc = new jsPDF();
-		doc.text(outputValue, 20, 20);
-		doc.save('DevStarCase.pdf');
+		doc.text(output, 20, 20);
+		doc.save('DevStarPalindrome.pdf');
 	}
-
-  
-	function copyToClipboard() {
-	  const textarea = document.querySelector("#output-textarea-id");
-	  const textareaElement = textarea as HTMLTextAreaElement;
-textareaElement.select();
-
-	  document.execCommand("copy");
-	}
-
 	
 </script>
 
@@ -128,107 +132,66 @@ textareaElement.select();
 <section class="bg-white dark:bg-gray-900">
 	<div class="py-8 px-4 mx-auto max-w-screen-xl lg:px-12">
 		<div class="card p-8 relative items-center mx-auto max-w-screen-xl overflow-hidden rounded-lg" id="box">
-			<select on:change={handleChange} 
-				class="rounded-lg bg-gray-50 text-gray-900 text-sm border-0 border-t-2 border-white focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-				<option>--Select Conversion-- </option>
-				{#each caseConversionOptions as option}
-				<option value={option}>{option}</option>
-			{/each}
-		</select>	
-			<div class="mt-3 gap-2 items-center mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 overflow-hidden">
+
+			<div class="rounded-lg overflow-hidden bg-gray-50 border border-gray-300" id="tarea1">
+				<select on:change={handleChange} 
+					class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+					<option>Select Conversion</option>
+					{#each caseConversionOptions as option}
+					<option value={option}>{option}</option>
+					{/each}
+				</select>	
+			</div>
+						
+			<div class="mt-3 gap-2 items-center mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 overflow-hidden" id="textbox">
 				
 				<div class="rounded-lg overflow-hidden bg-gray-50 border border-gray-300" id="tarea1">
-					<Textarea placeholder="Enter Text" id="input-textarea-id" rows="8" name="message"
+					<textarea placeholder="Enter Text" id="input-textarea-id" rows="8" name="message" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 					on:input={handleInput}/>
-					
 				</div>
 
 				<div class="rounded-lg overflow-hidden bg-gray-50 border border-gray-300" id="tarea2">
-					
-				<Textarea
-					placeholder="Result ...."
-					readOnly
-					id="output-textarea-id"
-					
-					rows="8"
-					
-					style="border-radius:0"
-					bind:value={outputValue}
-					/>
+					<textarea readOnly rows="8" placeholder="Result" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+					bind:value={output}/>
 				</div>
 
 			</div>
 
-			<div class="buttons" >
-				<Button color="blue" on:click={copyToClipboard}>Copy</Button>
-				<Button color="blue" on:click={downloadAsText}>Download as txt</Button>
+			<div id="buttonArea">
+				<Button color="blue" on:click={copyText}>Copy</Button>
+				<Button color="blue" on:click={downloadText}>Download as txt</Button>
 				<Button color="blue" on:click={downloadPDF}>Download as pdf</Button>
-			
-			  </div>
+			</div>
 
 		</div>
 	</div>
 </section>
 
-<style>
-	
-	
-	#box{
-		border:2px solid gray ;
-		padding:35px;
-	}
-	
-	#tarea1{
-		margin-right: 10px;
+<style>	
+
+	#textbox > div > textarea{
+		resize: none;
 	}
 
-	#tarea2{
-		margin-left: 10px;
+	#textbox{
+		margin-top: 20px;
+		gap:20px;
 	}
-	.buttons {
+
+	#buttonArea {
 		margin-top: 30px;
 		display: flex;
 		justify-content: center;
 		align-items: center;
-		gap: 30px;
-
+		gap: 30px; /* Adjust the gap between buttons as needed */
 	}
-/* 
-	@media screen and (max-width: 768px) {
-	  
-  
-	  .buttons {
-		margin-left: 0;
-	  }
+
+	.card {
+		box-shadow: rgba(0, 0, 0, 0.1) 0 0 0 2px;
 	}
-	@media screen and (max-width: 768px) {
-    #box {
-      padding: 15px;
-    }
 
-    select {
-      width: 100%;
-      margin-top: 10px; 
-    }
+	:is(.dark .card) {
+		box-shadow: rgba(255, 255, 255, 0.5) 0 0 0 2px;
+	}
 
-    .buttons {
-      flex-direction: column; 
-      align-items: center; 
-      margin-top: 20px;
-    }
-
-  }
-
-  @media screen and (max-width: 1024px) {
-    #box {
-      padding: 20px;
-    }
-  }
-
-  @media screen and (min-width: 1025px) {
-    #box {
-      padding: 30px;
-    }
-  } */
 </style>
-
