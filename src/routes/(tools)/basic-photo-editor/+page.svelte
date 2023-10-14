c49379c22c8c5d67960723d4c512b63134a90c68
rrjdk21 <98340850+rrjdk21@users.noreply.github.com>
2023-10-14 11:09:12 +0530

    negative_image

--- src/routes/(tools)/basic-photo-editor/+page.svelte
+++ src/routes/(tools)/basic-photo-editor/+page.svelte
@@ -1,100 +1,207 @@
 <svelte:head>
-	<title> Photo Editor</title>
+    <title>Photo Editor</title>
 </svelte:head>
+
 <script lang="ts">
-	import Intro from '$lib/Intro.svelte';
-	export let data;
-	
-	let uploadedImage;
-  let blurAmount = 0;
+    import { onMount } from 'svelte';
+    import { Label, Input } from 'flowbite-svelte';
+    import Intro from '$lib/Intro.svelte';
+    import Copy from '$lib/Copy.svelte';
 
-  async function handleFileInput(event) {
-    const file = event.target.files[0];
-    if (file && blurAmount !== undefined) {
-      const imageUrl = URL.createObjectURL(file);
-      const image = new Image();
-      image.src = imageUrl;
-      await image.decode(); // Ensure the image is loaded
-      
-      // Get a reference to the canvas element
-      const canvas = document.createElement('canvas');
-      const ctx = canvas.getContext('2d');
-      
-      // Set canvas dimensions to match the image
-      canvas.width = image.width;
-      canvas.height = image.height;
+    let redPercentage = 100;
+    let greenPercentage = 100;
+    let bluePercentage = 100;
+    let brightnessValue = 100;
+    let contrastValue = 100;
+    let displaySize = 40;
+    let isNegative = false;
+    let uploadedImage = null;
+    let originalImage = null;
+    let adjustments = { redPercentage, greenPercentage, bluePercentage, brightnessValue, contrastValue, displaySize };
+    let adjustmentStack = [];
+    let adjustmentIndex = -1;
+    let originalAdjustments = { ...adjustments };
 
-      // Apply blur effect
-      ctx.filter = `blur(${blurAmount}px)`;
-      ctx.drawImage(image, 0, 0);
-      
-      // Replace the uploadedImage with the blurred image data URL
-      uploadedImage = canvas.toDataURL();
+    export let data;
 
-      URL.revokeObjectURL(imageUrl); // Clean up the object URL
+    function handleImageUpload(event) {
+        const file = event.target.files[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = () => {
+                uploadedImage = reader.result;
+                originalImage = uploadedImage;
+                adjustments = { ...originalAdjustments };
+                applyAdjustments();
+                adjustmentStack = [];
+                adjustmentIndex = -1;
+            };
+            reader.readAsDataURL(file);
+        }
     }
-    
-  }
 
-  function downloadImage() {
-    if (uploadedImage) {
-      const link = document.createElement('a');
-      link.href = uploadedImage;
-      link.download = 'blurred_image.png';
-      link.click();
+    function applyAdjustments() {
+        const img = document.querySelector('#image-preview');
+
+        // Toggle negative effect
+        if (isNegative) {
+            img.style.filter = 'invert(100%)';
+        } else {
+            img.style.filter = `brightness(${adjustments.brightnessValue}%) contrast(${adjustments.contrastValue}%)`;
+        }
+
+        const canvas = document.createElement('canvas');
+        const ctx = canvas.getContext('2d');
+
+        const image = new Image();
+        image.src = uploadedImage;
+
+        image.onload = () => {
+            const displayWidth = (image.width * adjustments.displaySize) / 100;
+            const displayHeight = (image.height * adjustments.displaySize) / 100;
+
+            canvas.width = displayWidth;
+            canvas.height = displayHeight;
+
+            ctx.drawImage(image, 0, 0, displayWidth, displayHeight);
+
+            const imageData = ctx.getImageData(0, 0, displayWidth, displayHeight);
+            const data = imageData.data;
+
+            for (let i = 0; i < data.length; i += 4) {
+                data[i] = (data[i] * adjustments.redPercentage) / 100;
+                data[i + 1] = (data[i + 1] * adjustments.greenPercentage) / 100;
+                data[i + 2] = (data[i + 2] * adjustments.bluePercentage) / 100;
+            }
+
+            ctx.putImageData(imageData, 0, 0);
+            img.src = canvas.toDataURL('image/jpeg', 1.0);
+
+            if (adjustmentIndex === adjustmentStack.length - 1) {
+                adjustmentStack.push({ ...adjustments });
+                adjustmentIndex = adjustmentStack.length - 1;
+            }
+        };
     }
-  }
 
+    function resetToOriginal() {
+        if (originalImage) {
+            adjustments = { ...originalAdjustments };
+            applyAdjustments();
+        }
+    }
+
+    function undo() {
+        if (adjustmentIndex > 0) {
+            adjustmentIndex--;
+            const previousAdjustments = adjustmentStack[adjustmentIndex];
+            Object.assign(adjustments, previousAdjustments);
+            applyAdjustments();
+        }
+    }
+
+    function redo() {
+        if (adjustmentIndex < adjustmentStack.length - 1) {
+            adjustmentIndex++;
+            const nextAdjustments = adjustmentStack[adjustmentIndex];
+            Object.assign(adjustments, nextAdjustments);
+            applyAdjustments();
+        }
+    }
+
+    function downloadImage() {
+        const link = document.createElement('a');
+        link.href = uploadedImage;
+        link.download = 'adjusted_image.jpg';
+        link.click();
+    }
 </script>
 
 <Intro heading={data.meta.title} description={data.meta.description} />
 
-<main>
-	<h1 class="text-white">Image Blur Effect</h1>
-  
-	<input
-	  type="file"
-	  accept="image/*"
-	  on:change={handleFileInput}
-	  class="text-white"
-	/>
-  
-	<label class="text-white">
-	  Blur Amount:
-	  <input
-		type="range"
-		min="0"
-		max="10"
-		step="0.5"
-		bind:value={blurAmount}
-	  />
-	  {blurAmount} pixels
-	</label>
-  
-	{#if uploadedImage}
-	<div class="image-container">
-		<h2 class="text-white">Blurred Image</h2>
-		<img src={uploadedImage} alt="Blurred Image"/>
-		<button on:click={downloadImage} class="mt-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Download Blurred Image</button>
-	</div>
-	{/if}
-
-</main>
-
-
 <style>
-	main {
-    text-align: center;
-    margin: 20px;
-  }
-  img {
-    max-width: 100%;
-    margin-top: 10px;
-  }	
-  .image-container {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    text-align: center;
-  }
+    .container {
+        display: flex;
+        justify-content: space-between;
+    }
+    .image-container {
+        width: 60%;
+    }
+    .adjusters {
+        width: 30%;
+    }
+    .adjuster-label {
+        margin-top: 10px;
+    }
+    .adjuster-input {
+        width: 100%;
+    }
+    button {
+        background-color: #333; /* Blackish grey color */
+        color: white;
+        border: none;
+        padding: 10px 20px;
+        margin: 5px;
+        cursor: pointer;
+    }
 </style>
+
+<input type="file" accept="image/*" on:change={handleImageUpload} style="background-color: #333; color: white; border: none; padding: 10px 20px; margin: 5px; cursor: pointer;" />
+
+<div class="container">
+    <div class="image-container">
+        <img id="image-preview" src="" alt="Uploaded Image" />
+    </div>
+
+    <div class="adjusters">
+        <div class="adjuster-label">
+            <label for="redPercentage">Red (%):</label>
+        </div>
+        <div class="adjuster-input">
+            <input type="range" id="redPercentage" bind:value={adjustments.redPercentage} min="0" max="200" step="1" on:input={applyAdjustments} />
+        </div>
+        <div class="adjuster-label">
+            <label for="greenPercentage">Green (%):</label>
+        </div>
+        <div class="adjuster-input">
+            <input type="range" id="greenPercentage" bind:value={adjustments.greenPercentage} min="0" max="200" step="1" on:input={applyAdjustments} />
+        </div>
+        <div class="adjuster-label">
+            <label for="bluePercentage">Blue (%):</label>
+        </div>
+        <div class="adjuster-input">
+            <input type="range" id="bluePercentage" bind:value={adjustments.bluePercentage} min="0" max="200" step="1" on:input={applyAdjustments} />
+        </div>
+        <div class="adjuster-label">
+            <label for="brightnessValue">Brightness:</label>
+        </div>
+        <div class="adjuster-input">
+            <input type="range" id="brightnessValue" bind:value={adjustments.brightnessValue} min="0" max="200" step="1" on:input={applyAdjustments} />
+        </div>
+        <div class="adjuster-label">
+            <label for="contrastValue">Contrast:</label>
+        </div>
+        <div class="adjuster-input">
+            <input type="range" id="contrastValue" bind:value={adjustments.contrastValue} min="0" max="200" step="1" on:input={applyAdjustments} />
+        </div>
+        <div class="adjuster-label">
+            <label for="displaySize">Display Size (%):</label>
+        </div>
+        <div class="adjuster-input">
+            <input type="range" id="displaySize" bind:value={adjustments.displaySize} min="10" max="100" step="1" on:input={applyAdjustments} />
+        </div>
+        <div class="adjuster-label">
+            <label for="isNegative">Negative Image:</label>
+        </div>
+        <div class="adjuster-input">
+            <input type="checkbox" id="isNegative" bind:checked={isNegative} on:change={applyAdjustments} />
+        </div>
+    </div>
+</div>
+
+<div>
+    <button on:click={resetToOriginal} disabled={!originalImage}>Reset to Original</button>
+    <button on:click={undo} disabled={adjustmentIndex <= 0}>Undo</button>
+    <button on:click={redo} disabled={adjustmentIndex >= adjustmentStack.length - 1}>Redo</button>
+    <button on:click={downloadImage}>Download Adjusted Image</button>
+</div>
