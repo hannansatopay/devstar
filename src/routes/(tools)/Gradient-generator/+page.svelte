120cde3162253f0bb405d26cbe9aec68fe56fa79
Yash Mankar <yash.mankar10122003@gmail.com>
2023-10-14 23:38:21 +0530

    More Option

--- src/routes/(tools)/Gradient-generator/+page.svelte
+++ src/routes/(tools)/Gradient-generator/+page.svelte
@@ -2,31 +2,33 @@
   import Intro from '$lib/Intro.svelte'
   import { Label, Input } from 'flowbite-svelte';
   import { identity } from 'svelte/internal';
-  import { Button, Dropdown, DropdownItem } from 'flowbite-svelte';
+  import { Button, Dropdown, DropdownItem, DropdownDivider} from 'flowbite-svelte';
   import ColorPicker from 'svelte-awesome-color-picker';
+  import { Popover } from 'flowbite-svelte';
+  import { onMount } from 'svelte';
 
   export let data;
 
-  let Types = ['Linear','Radial'];
+  let Types = ['linear','radial'];
   let percentages = [0,10,20,30,40,50,60,70,80,90,100];
   let Rotations = [0,45,90,135,180,225,270,315,360];
   
-  let gradientCode = "";
   let gradienttype = `${Types[0]}`;
-  let gradientrotation = `0`;
-  let gradientColors = ["#AE2828","#000000"];
+  let gradientrotation = `${Rotations[0]}`;
+  let gradientColors = ['',''];
   let gradientper = `${percentages[0]}`;
+  let gradientCode = `Linear-gradient(${gradientrotation}deg ,${gradientColors[0]}, ${gradientColors[1]})`;
 
   function generateGradient() {
-  gradientCode = `linear-gradient(45deg, ${gradientColors[0]}, ${gradientColors[1]})`;
-}
+      gradientCode = `Linear-gradient(${gradientrotation}deg ,${gradientColors[0]}, ${gradientColors[1]})`;
+    }
+
 
  function generateRandomGradient() {
   const r = Math.floor(Math.random() * 256).toString(16).padStart(2, '0');
   const g = Math.floor(Math.random() * 256).toString(16).padStart(2, '0');
   const b = Math.floor(Math.random() * 256).toString(16).padStart(2, '0');
 
-// Combine the three bytes into a hexadecimal color value.
   const color1 = `#${r}${g}${b}`;
   const color2 = `#${g}${b}${r}`;
 
@@ -41,13 +43,16 @@
 const copyCode = async () => {
     try {
       await navigator.clipboard.writeText(gradientCode);
-      alert("CSS Code: " + "background: "+ gradientCode);
+      console.log("CSS Copied");
     } catch (error) {
       console.error("Error copying text to clipboard:", error);
     }
   }
+
+  onMount(generateRandomGradient);
 </script>
 
+<!-- svelte-ignore non-top-level-reactive-declaration -->
 <Intro heading={data.meta.title} description={data.meta.description}/>
 
 <div class="bg-white dark:bg-gray-900">
@@ -90,8 +95,8 @@ const copyCode = async () => {
       <Label class="text-gray-700 dark:text-gray-400">Rotation:</Label>
         <div class="flex space-x-2">
           <select bind:value={gradientrotation} class="text-gray-900 bg-white col-sm border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
-            {#each Rotations as Rotation}
-                <option>{Rotation}&#176;</option>
+            {#each Rotations as Rotation,index}
+                <option>{Rotation}</option>
             {/each}
             </select>
         </div>
@@ -101,7 +106,7 @@ const copyCode = async () => {
         <div class="flex space-x-2">
           <select bind:value={gradientper} class="text-gray-900 bg-white col-sm border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
             {#each percentages as percentage}
-                <option>{percentage}%</option>
+                <option>{percentage}</option>
             {/each}
             </select>
         </div>
@@ -129,12 +134,29 @@ const copyCode = async () => {
 
         <!-- Copy CSS Button -->
         <button
+        id="b1"
           type="button"
           class="text-white bg-green-500 hover:bg-green-600 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 mt-2"
           on:click={copyCode}
         >
-          Copy CSS
+            Copy CSS
         </button>
+        <Popover class="w-64 text-sm font-light " title="CSS Output" triggeredBy="#b1">
+          background: {gradientCode}
+        </Popover>
+
+        <button
+          type="button"
+          class="text-white bg-green-500 hover:bg-green-600 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 mt-2"
+        >
+            More
+        </button>
+        <Dropdown>
+          <DropdownItem on:click={copyCode}>Copy</DropdownItem>
+          <DropdownDivider />
+          <DropdownItem>Fullscreen</DropdownItem>
+          <DropdownItem>Download</DropdownItem>
+        </Dropdown>
     </div>
 
     <!-- Gradient Display (Right Half) {gradientCode}-->
@@ -142,9 +164,8 @@ const copyCode = async () => {
       <div
         class="w-full h-64"
         style="background-Image: {gradientCode};"
-      ></div>
-      <Label class="text-gray-700 dark:text-gray-400">{gradientCode}</Label>
-    </div>
+      >
+      </div>
   </div>
 </main>
 </div>
