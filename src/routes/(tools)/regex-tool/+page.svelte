16b01ba41e75e1bac5d32d0e4e706947291cd3aa
pajju <prajwalshah.2003@gmail.com>
2023-10-14 16:49:52 +0530

    css updated

--- src/routes/(tools)/regex-tool/+page.svelte
+++ src/routes/(tools)/regex-tool/+page.svelte
@@ -13,7 +13,7 @@
 		console.log(data);
 	};
 	function findMatchingPatterns() {
-        if(regexPattern == "") return;
+		if (regexPattern == '') return;
 		const regex = new RegExp(regexPattern, 'g');
 		const matches = testText.match(regex);
 		updateHighlightedText(matches?.length);
@@ -24,8 +24,8 @@
 		}
 	}
 	function updateHighlightedText(len) {
-        if(len == 0) return;
-        console.log(len)
+		if (len == 0) return;
+		console.log(len);
 		highlightedText = testText.replace(
 			new RegExp(regexPattern, 'g'),
 			`<div class="bg-blue-200 text-black inline-block font-semibold opacity-75 h-6">&nbsp;$& &nbsp;</div>`
@@ -38,30 +38,46 @@
 <section class="bg-white dark:bg-gray-900 flex justify-center">
 	<div class="border border-white h-96 mb-2 w-2/3">
 		<div class="regex-Header">
-			<div class="hover:dark:bg-gray-900 " on:click={() => (language = 'Python')}>Python</div>
-			<div class="hover:dark:bg-gray-900" on:click={() => (language = 'Javascript')}>
+			<div
+				class="bg-gray-100 text-black dark:bg-gray-900 dark:text-gray-100"
+				on:click={() => (language = 'Python')}
+			>
+				Python
+			</div>
+			<div
+				class="bg-gray-100 text-black dark:bg-gray-900 dark:text-gray-100"
+				on:click={() => (language = 'Javascript')}
+			>
 				Javascript
 			</div>
-			<div class="hover:dark:bg-gray-900" on:click={() => (language = 'PHP')}>PHP</div>
+			<div
+				class="bg-gray-100 text-black dark:bg-gray-900 dark:text-gray-100"
+				on:click={() => (language = 'PHP')}
+			>
+				PHP
+			</div>
 		</div>
 		<div class="regex-container flex flex-col">
 			<input
-				class="w-1/3 m-4 rounded-md bg-gray-800 placeholder-gray-500 focus:placeholder-gray-400"
+				class="text-gray-700 bg-gray-100 dark:text-white w-1/3 m-4 rounded-md dark:bg-gray-800 placeholder-gray-500 focus:placeholder-gray-400"
 				type="text"
 				bind:value={regexPattern}
 				placeholder="Enter your {language} regular expression here"
 			/>
 			<input
-				class="w-3/3 h-20 m-4 flex justify-center text-center rounded-md bg-gray-800 placeholder-gray-500 focus:placeholder-gray-400 color-gray-400"
+				class="text-gray-700 bg-gray-100 dark:text-white w-3/3 h-20 m-4 flex justify-center text-center rounded-md dark:bg-gray-800 placeholder-gray-500 focus:placeholder-gray-400 color-gray-400"
 				type="text"
 				bind:value={testText}
 				on:change={handleTextChange}
 				placeholder="Input your text for regex testing here :)"
 			/>
-			<button on:click={findMatchingPatterns}
-			class="w-1/3 m-2 text-white border border-white rounded-md bg-gray-800 placeholder-gray-500 focus:placeholder-gray-400 relative left-2">Test</button>
+			<button
+				on:click={findMatchingPatterns}
+				class="w-1/3 m-2 bg-gray-100 text-gray-700 dark:text-white border border-gray-500 rounded-md dark:bg-gray-800 placeholder-gray-500 focus:placeholder-gray-400 relative left-2"
+				>Test</button
+			>
 			<div
-				class="w-3/3 h-20 m-4 text-white border border-white flex justify-center text-center rounded-md bg-gray-800 placeholder-gray-500 focus:placeholder-gray-400 color-gray-400"
+				class="w-3/3 h-20 m-4 bg-gray-100 flex justify-center text-center rounded-md text-gray-700 border border-gray-500 dark:bg-gray-800 placeholder-gray-500 focus:placeholder-gray-400"
 			>
 				{@html highlightedText}
 			</div>
@@ -77,14 +93,12 @@
 	}
 	.regex-Header {
 		height: 3rem;
-		background-color: #172334;
+		/* background-color: #172334; */
 		display: flex;
 	}
-	input {
-		color: white;
-	}
+
 	.regex-Header > div {
-		color: lightgray;
+		/* color: lightgray; */
 		display: flex;
 		align-items: center;
 		justify-content: center;
