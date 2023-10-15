762f668246875be7eda891ae1b5d942679a1a6d8
ruchira30 <108223104+ruchira30@users.noreply.github.com>
2023-10-15 22:43:27 +0530

    Update String tools and Delete other tools

--- src/routes/tools.js
+++ src/routes/tools.js
@@ -100,5 +100,16 @@ export let tools = {
         icon: 'files', // Pending Icon Update
         link: '/random-data-generator',
         description: 'The Random Data Generator is a tool which generates fake/dummy dataset that can be used for testing.'
-    }
-}
+    },
+    'line-sorter': {
+        name: 'Line Sorter',
+        link: '/line-sorter',
+        description: 'Line Sorter – the magical tool that transforms chaos into order with a single click! Whether you want to arrange your words alphabetically for a touch of elegance, unveil a secret message through reversal, bring the excitement of randomness into your text, or even sort words by their unique lengths.Experience the art of line sorting!'
+    },
+    
+    'string-truncator': {
+        name: 'String Truncator',
+        link: '/string-truncator',
+        description: 'Easily shorten your text with our String Truncator Tool. Specify a character limit, and it will trim your content. Ideal for concise social media posts and emails'
+    },
+};
