f35f9d5e8cd0b02d24b19d4acc28528b7197de9d
hynorm972 <iyer.v@somaiya.edu>
2023-10-15 15:35:45 +0530

    updated QR code generation

--- src/routes/(tools)/qr-generator/+page.svelte
+++ src/routes/(tools)/qr-generator/+page.svelte
@@ -1,55 +1,55 @@
 <script>
-	let inputText = "";
-	let textPresent = false;
-	let API_URL = 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=';
-	function dataSubmit(e){
-		e.preventDefault();
-		if(inputText !== ""){
-			textPresent = true;
-			API_URL+=inputText;
-		}
-	}
+    let inputText = "";
+    let textPresent = false;
+    let API_URL = 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=';
+    function dataSubmit(e){
+        e.preventDefault();
+        if(inputText !== ""){
+            textPresent = true;
+            API_URL += inputText;
+        }
+    }
 </script>
 
 <style>
-	img{
-		margin-top: 100px;
-		margin-bottom: 30px;
-	}
-	a{
-		font-weight: 700px;
-		font-size: 30px;
-		color: black;
-	}
-	#textInput{
-		width: 70%;
-		height: 40px;
-		color: #484848;
-		border-width: 1.5px;
-		border-style: solid;
-		border-color: black;
-		padding: 3px;
-		font-weight: 700;
-	}
-	#btn{
-		border-radius: 3px;
-		background-color: black;
-		color: whitesmoke;
-		font-weight: 700;
-		cursor: pointer;
-	}
+    img {
+        margin-top: 100px;
+        margin-bottom: 30px;
+    }
+    a {
+        font-weight: 700px;
+        font-size: 30px;
+        color: black;
+    }
+    #textInput {
+        width: 70%;
+        height: 40px;
+        color: #484848;
+        border-width: 1.5px;
+        border-style: solid;
+        border-color: black;
+        padding: 3px;
+        font-weight: 700;
+    }
+    #btn {
+        border-radius: 3px;
+        background-color: black;
+        color: whitesmoke;
+        font-weight: 700;
+        cursor: pointer;
+    }
 </style>
 
 <div>
-<center>
-<h1>QR CODE GENERATOR</h1>
-<form on:submit={dataSubmit}>
-<input id="textInput" type="text" placeholder="Enter any text or url..." bind:value={inputText}/>
-<input id="btn" type="submit">
-</form>
-{#if textPresent}
-	<img src={API_URL} /><br>
-	<a href={API_URL} download>Download</a> 
-{/if}
-</center>
+    <center>
+        <h1>QR CODE GENERATOR</h1>
+        <form on:submit={dataSubmit}>
+            <input id="textInput" type="text" placeholder="Enter any text or URL..." bind:value={inputText} />
+            <input id="btn" type="submit">
+        </form>
+        {#if textPresent}
+            <img src={API_URL} alt="QR Code" /><br>
+            <a href={API_URL} download>Download</a>
+        {/if}
+    </center>
 </div>
--- src/routes/tools.js
+++ src/routes/tools.js
@@ -95,7 +95,7 @@ export let tools = {
         link: '/robots-txt-generator',
         description: "The Robots.txt Generator is a must-have tool for developers to create the robots.txt file, which controls search engine crawlers' access to specific pages or directories on a website. This tool offers a user-friendly interface to define crawling rules, including allow and disallow directives, to ensure optimal search engine indexing and prevent unwanted content from being indexed. Create and customize your robots.txt file efficiently with this generator to enhance your website's search engine optimization (SEO) efforts."
     },
-    'QR-Generator': {
+    'qr-generator': {
         name: 'QR Code Generator',
         icon: 'spider-web',
         link: '/qr-generator',
