e4977b0b1b9299da53901f082d3d5d2940b89387
Sumitm6879 <sm78810@gmail.com>
2023-10-17 08:39:00 +0530

    Annuity Payout Calculator

--- src/routes/tools.js
+++ src/routes/tools.js
@@ -94,5 +94,11 @@ export let tools = {
         icon: 'spider-web',
         link: '/robots-txt-generator',
         description: "The Robots.txt Generator is a must-have tool for developers to create the robots.txt file, which controls search engine crawlers' access to specific pages or directories on a website. This tool offers a user-friendly interface to define crawling rules, including allow and disallow directives, to ensure optimal search engine indexing and prevent unwanted content from being indexed. Create and customize your robots.txt file efficiently with this generator to enhance your website's search engine optimization (SEO) efforts."
+    },
+    'annuity-payout-calculator': {
+        name: 'Annuity Payout Calculator',
+        icon: 'arrow-right-arrow-left',
+        link: '/annuity-payout-calculator',
+        description: "This calculator can estimate the annuity payout amount for a fixed payout length or estimate the length that an annuity can last if supplied a fixed payout amount. Please use our Annuity Calculator to estimate the end balance of an annuity for the accumulation phase."
     }
 };
