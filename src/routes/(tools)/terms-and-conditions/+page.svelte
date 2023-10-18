793c460175557f4bc9541b7756f274ad5ab90473
Dinesh Jonnalagadda <106725225+Dineshjnld@users.noreply.github.com>
2023-10-18 22:37:25 +0530

    Update +page.svelte

--- src/routes/(tools)/terms-and-conditions/+page.svelte
+++ src/routes/(tools)/terms-and-conditions/+page.svelte
@@ -1,91 +1,219 @@
-<section class="py-8 bg-white lg:py-24 dark:bg-gray-900">
-	<div class="max-w-5xl px-4 mx-auto lg:px-4 font-light text-dark sm:text-lg dark:text-white">
-		<h1 class="mb-6 text-3xl font-bold text-gray-900 lg:text-4xl dark:text-white">
-			Terms and conditions
-		</h1>
-		<p class="mb-10 text-lg text-gray-600 dark:text-gray-400 lg:text-lg">
-			Read about the terms and conditions for Flowbite. Have a question? Feel free to <a class="font-normal text-blue-600 no-underline dark:text-blue-400 hover:underline" href="/contact">contact us</a> and we will help you out.
-		</p>
-		<hr class="my-12 border-gray-200 dark:border-gray-800" />
-		<p>Please read these Terms and Conditions ("Terms") carefully before accessing or using the Developerstar ("Platform") accessible via <a href="https://www.developerstar.com">www.developerstar.com</a>. These Terms constitute a legal agreement between you ("User," "you," or "your") and Developerstar ("Company," "we," "us," or "our") regarding your use of the Platform. By accessing or using the Platform, you agree to be bound by these Terms. If you do not agree with any part of these Terms, you should not access or use the Platform.</p>
-		<h2 class="mt-4 font-medium">1. Definitions</h2>
-		<p>
-			<strong>1.1. "Platform"</strong> refers to the Developerstar Platform provided by the Company, including all associated websites, applications, software, tools, and services accessible via <a href="https://www.developerstar.com">www.developerstar.com</a>.<br>
-			<strong>1.2. "User"</strong> refers to any individual or entity accessing or using the Platform.<br>
-			<strong>1.3. "Content"</strong> refers to any information, data, text, images, graphics, code, or other materials uploaded, downloaded, or accessed through the Platform.<br>
-			<strong>1.4. "Tools"</strong> refer to the tools available on <a href="https://www.developerstar.com">www.developerstar.com</a>.
-		</p>
+<script lang="ts">
+	import Intro from '$lib/Intro.svelte';
 
-		<h2 class="mt-4 font-medium">2. Registration and Account</h2>
-		<p>
-			<strong>2.1.</strong> In order to use certain features of the Platform, you may be required to register for an account.<br>
-			<strong>2.2.</strong> You agree to provide accurate, complete, and up-to-date information during the registration process and to keep your account credentials secure and confidential.<br>
-			<strong>2.3.</strong> You are solely responsible for all activities that occur under your account.
-		</p>
+	export let data;
 
-		<h2 class="mt-4 font-medium">3. License Grant</h2>
-		<p>
-			<strong>3.1.</strong> Subject to compliance with these Terms, the Company grants you a limited, non-exclusive, non-transferable, and revocable license to access and use the Platform for the purpose of developing applications and software.<br>
-			<strong>3.2.</strong> You acknowledge that the Company retains all intellectual property rights in the Platform, and you shall not use, copy, modify, distribute, or create derivative works based on the Platform without the Company's prior written consent.
-		</p>
+	// start
+	let companyName = '';
+	let websiteName = '';
+	let websiteUrl = '';
+	let email = '';
+	let showTerms = true;
 
-		<h2 class="mt-4 font-medium">4. User Obligations</h2>
-		<p>
-			<strong>4.1.</strong> You shall comply with all applicable laws, regulations, and these Terms while using the Platform.<br>
-			<strong>4.2.</strong> You shall not use the Platform for any unlawful, fraudulent, or unauthorized purposes.<br>
-			<strong>4.3.</strong> You shall not upload, post, transmit, or otherwise make available any Content that is unlawful, harmful, defamatory, infringing, or violates any third-party rights.<br>
-			<strong>4.4.</strong> You are solely responsible for the Content you upload, and you grant the Company a non-exclusive, worldwide, royalty-free license to use, reproduce, modify, adapt, publish, translate, distribute, and display such Content for the purpose of providing the Platform and improving its features.
-		</p>
 
-		<h2 class="mt-4 font-medium">5. Ownership of User-Generated Content</h2>
-		<p>
-			The Company acknowledges that it will not retain any rights, titles, or interests in the text or documents you submit using the Tools available on <a href="https://www.developerstar.com">www.developerstar.com</a>.
-		</p>
+	let termsAndPolicy = '';
 
-		<h2 class="mt-4 font-medium">6. Third-Party Services and Content</h2>
-		<p>
-			<strong>6.1.</strong> The Platform may integrate with third-party services, tools, or content. Your use of such services or content is subject to their respective terms and conditions, and the Company shall not be responsible or liable for any third-party services or content.<br>
-			<strong>6.2.</strong> The inclusion of any third-party services or content within the Platform does not imply endorsement or affiliation with such third parties.
-		</p>
+	function handleSubmit() {
+		// Capture the user's input
+		companyName = companyName.trim();
+		websiteName = websiteName.trim();
+		websiteUrl = websiteUrl.trim();
+		email = email.trim();
 
-		<h2 class="mt-4 font-medium">7. Termination</h2>
-		<p>
-			<strong>7.1.</strong> The Company reserves the right to suspend, restrict, or terminate your access to the Platform at any time without notice or liability, for any reason or no reason, in its sole discretion.<br>
-			<strong>7.2.</strong> Upon termination, your license to use the Platform shall immediately cease, and you shall delete any downloaded or stored Content from the Platform.
-		</p>
+		const companyNameBold = `<strong>${companyName}</strong>`;
 
-		<h2 class="mt-4 font-medium">8. Disclaimer of Warranties</h2>
-		<p>
-			<strong>8.1.</strong> The Platform is provided on an "as is" and "as available" basis. The Company makes no representations or warranties of any kind, whether express, implied, or statutory, regarding the Platform, including but not limited to any warranties of merchantability, fitness for a particular purpose, or non-infringement.<br>
-			<strong>8.2.</strong> The Company does not warrant that the Platform will be uninterrupted, error-free, secure, or free from viruses or other harmful components.
-		</p>
+		// Generate the terms and policy content
+		termsAndPolicy = `
+<div>
+  <strong>Terms and Conditions</strong>
+</div>
+<br>
+<div>
+  Welcome to <span>${companyNameBold}</span>!
+</div>
+<div>
+  These terms and conditions outline the rules and regulations for the use of <span>${companyNameBold}'s</span> Website, located at <span>${websiteUrl}</span>.
+</div>
+<br>
 
-		<h2 class="mt-4 font-medium">9. Limitation of Liability</h2>
-		<p>
-			<strong>9.1.</strong> To the maximum extent permitted by applicable law, the Company shall not be liable for any indirect, incidental, special, consequential, or punitive damages arising out of or relating to your use or inability to use the Platform, including but not limited to loss of profits, data, or goodwill.<br>
-			<strong>9.2.</strong> In no event shall the Company's total liability exceed the amount you have paid, if any, to access the Platform.
-		</p>
+<div>
+  By accessing this website, we assume you accept these terms and conditions. Do not continue to <span>${companyNameBold}</span> if you do not agree to take all of the terms and conditions stated on this page.
+</div>
+<br>
+<div>
+  <strong>Definitions</strong>
+</div>
+<br>
+<div>
+  The following terminology applies to these Terms and Conditions:
+</div><br>
 
-		<h2 class="mt-4 font-medium">10. Indemnification</h2>
-		<p>
-			You agree to indemnify, defend, and hold harmless the Company, its officers, directors, employees, and agents from and against any and all claims, liabilities, damages, losses, costs, or expenses, including reasonable attorneys' fees, arising out of or in connection with your use of the Platform or violation of these Terms.
-		</p>
+<div>
+  <span>"Client," "You," and "Your"</span> refers to you, the individual accessing this website and agreeing to comply with the terms and conditions.
+</div>
+<div>
+  <span>"The Company," "Ourselves," "We," "Our," and "Us"</span> refers to our Company.
+</div>
+<div>
+  <span>"Party," "Parties," or "Us"</span> refers to both the Client and ourselves.
+</div>
+<div>
+  All terms used herein, whether singular, plural, capitalized, or not, are considered interchangeable.
+</div><br>
 
-		<h2 class="mt-4 font-medium">11. Modifications</h2>
-		<p>
-			<strong>11.1.</strong> The Company reserves the right to modify or update these Terms at any time, and the updated version will be effective upon posting on the Platform.<br>
-			<strong>11.2.</strong> It is your responsibility to review these Terms periodically to ensure your continued compliance.<br>
-			<strong>11.3.</strong> By continuing toaccess or use the Platform after any modifications, you agree to be bound by the revised Terms.
+<div>
+  <strong>Cookies</strong>
+</div><br>
+<div>
+  We employ the use of cookies. By accessing <span>${companyNameBold}</span>, you agree to use cookies in agreement with <span>${companyNameBold}'s</span> Privacy Policy.
+</div><br>
+<div>
+  <strong>License</strong>
+</div><br>
+<div>
+	Unless otherwise stated,<span>${companyNameBold}</span> and/or its licensors own the intellectual property rights for all material on <span>${companyNameBold}</span>. All intellectual property rights are reserved. You may access this content from <span>${companyNameBold}</span> for personal use, subject to the restrictions specified in these terms and conditions.</div>
+<div>
+<div>You must not:</div>
+<br>
+<ul>
+  <li>Republish material from ${companyNameBold}.</li>
+  <li>Sell, rent, or sub-license material from ${companyNameBold}.</li>
+  <li>Reproduce, duplicate, or copy material from ${companyNameBold}.</li>
+  <li>Redistribute content from ${companyNameBold}.</li>
+</ul>
+<br>
+<div>
+<strong>Content Liability</strong>
+</div>
+<br>
+<div> We shall not be responsible for any content appearing on your Website. You agree to protect and defend us against all claims that arise on your Website. No links should appear on any Website that could be interpreted as libelous, obscene, or criminal or that infringes, violates, or advocates the infringement or other violation of any third-party rights.
+</div>
+<div>
+<strong>Termination</strong>
+</div>
+<br>
+<div> The Company reserves the right to suspend, restrict, or terminate your access to the Platform at any time without notice or liability, for any reason or no reason, in its sole discretion.
+</div>
+<br>
+<div>
+  <strong>Contact Us</strong>
+</div>
+<div>
+	If you have any questions or concerns about these Terms and Conditions or any other matter, you can contact us at <strong>${email}</strong> .
+</div>
+    `;
+		showTerms = false;
+	}
 
-		<h2 class="mt-4 font-medium">12. Governing Law and Jurisdiction</h2>
-		<p>
-			<strong>12.1.</strong> These Terms shall be governed by and construed in accordance with the laws of Mumbai, India.<br>
-			<strong>12.2.</strong> Any disputes arising out of or in connection with these Terms shall be subject to the exclusive jurisdiction of the courts of Mumbai, India.
-		</p>
+	function copyToClipboard() {
+		// Create a new textarea element to hold the text
+		const textarea = document.createElement('textarea');
+		textarea.value = termsAndPolicy;
+		document.body.appendChild(textarea);
 
-		<h2 class="mt-4 font-medium">13. Severability</h2>
-		<p>
-			<strong>13.1.</strong> If any provision of these Terms is held to be invalid, illegal, or unenforceable, the validity, legality, or enforceability of the remaining provisions shall not in any way be affected or impaired.
-		</p>
-	</div>
-</section>
+		// Select the text inside the textarea
+		textarea.select();
+
+		// Copy the selected text to the clipboard
+		document.execCommand('copy');
+
+		// Remove the textarea from the DOM
+		document.body.removeChild(textarea);
+
+		alert('Terms and Conditions copied to clipboard!');
+	}
+</script>
+
+<Intro heading={data.meta.title} description={data.meta.description} />
+
+<div class=" bg-[#323A49] mt-2 w-1/2 container mx-auto py-8 flex flex-col items-center">
+	<h1 class="text-3xl comp font-semibold mb-4 tw-text-red">Company Information</h1>
+	<form on:submit={handleSubmit} class="space-y-4">
+		<div class="flex flex-col">
+			<label for="companyName" class="text-lg text-white">Company Name:</label>
+			<input
+				type="text"
+				id="companyName"
+				bind:value={companyName}
+				class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+				required
+			/>
+		</div>
+		<div class="flex flex-col">
+			<label for="websiteName" class="text-lg text-white">Website Name:</label>
+			<input
+				type="text"
+				id="websiteName"
+				bind:value={websiteName}
+				class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+				required
+			/>
+		</div>
+		<div class="flex flex-col">
+			<label for="websiteUrl" class="text-lg text-white">Website URL:</label>
+			<input
+				type="text"
+				id="websiteUrl"
+				bind:value={websiteUrl}
+				class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+				required
+			/>
+		</div>
+		<div class="flex flex-col">
+			<label for="email" class="text-lg text-white">Enter your Email</label>
+			<input
+				type="email"
+				id="email"
+				bind:value={email}
+				class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+				placeholder="john.doe@company.com"
+				required
+			/>
+		</div>
+		<button
+			type="submit"
+			class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+		>
+			Generate My Terms & Conditions
+			<svg
+				class="w-3.5 h-3.5 ml-2"
+				aria-hidden="true"
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 14 10"
+				><path
+					stroke="currentColor"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="M1 5h12m0 0L9 1m4 4L9 9"
+				/>
+			</svg>
+		</button>
+	</form>
+</div>
+
+<div class=" result w-1/2 content flex flex-col items-center justify-center mx-auto mt-8">
+	{#if !showTerms}
+		<button
+			type="button"
+			on:click={copyToClipboard}
+			class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+			>Copy</button
+		>
+	{/if}
+	{#if showTerms}
+		<h2>Terms & Conditions Will go here</h2>
+	{/if}
+	<div id="termsAndPolicyContainer" class="result">{@html termsAndPolicy}</div>
+</div>
+
+<style>
+	.content {
+		padding: 20px;
+		background-color: lightgray;
+	}
+	.comp {
+		color: white;
+	}
+</style>
