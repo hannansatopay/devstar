83bb7b49807bbbcd36dccd4e8e107b2a0da8bc1d
Kalpak Goshikwar <kalpakgoshikwar123@gmail.com>
2023-10-24 12:25:10 +0530

    New Conic gradient animation

--- src/routes/(tools)/living-background-generator/+page.svelte
+++ src/routes/(tools)/living-background-generator/+page.svelte
@@ -1,79 +1,79 @@
-<script lang="ts">
-    import Intro from '$lib/Intro.svelte';
-    import { Label, Input, Range, Radio, Checkbox } from 'flowbite-svelte';
-    import { onMount } from 'svelte';
-    import Copy from '$lib/Copy.svelte';
+<script lang="ts" defer>
+	import Intro from '$lib/Intro.svelte';
+	import { Label, Input, Range, Radio, Checkbox } from 'flowbite-svelte';
+	import { onMount } from 'svelte';
+	import Copy from '$lib/Copy.svelte';
 
-    export let data;
-    let innerWidth: any = null;
-    let innerHeight: any = null;
-    let screenWidth: any = null;
-    let screenHeight: any = null;
-    let css = '';
+	export let data;
+	let innerWidth: any = null;
+	let innerHeight: any = null;
+	let screenWidth: any = null;
+	let screenHeight: any = null;
+	let css = '';
 
-    onMount(() => {
-        screenWidth = screen.width;
-        screenHeight = screen.height;
-        colorInput = document.getElementById('color-input');
-        alphaSlider = document.getElementById('alpha-slider');
-        colorDisplay = document.getElementById('color-display');
-    });
+	onMount(() => {
+		screenWidth = screen.width;
+		screenHeight = screen.height;
+		colorInput = document.getElementById('color-input');
+		alphaSlider = document.getElementById('alpha-slider');
+		colorDisplay = document.getElementById('color-display');
+	});
 
-    let clrList = ['#000000', '#bb2d6f', '#fd9d1d', '#fcf437'];
-    let clrVal3 = '#000';
-    let isRandom = false; // A flag to toggle random colors
+	let clrList = ['#000000', '#bb2d6f', '#fd9d1d', '#fcf437'];
+	let clrVal3 = '#000';
+	let isRandom = false; // A flag to toggle random colors
 
-    const pushArr = () => {
-        clrList.length < 6 ? (clrList = [...clrList, clrVal3]) : alert('Too many colors');
-    };
+	const pushArr = () => {
+		clrList.length < 6 ? (clrList = [...clrList, clrVal3]) : alert('Too many colors');
+	};
 
-    //remove the div on click
+	//remove the div on click
 
-    const removeOnClick = (e) => {
-        clrList = clrList.filter((x, y) => x !== e.target.id);
-        console.log(e.target.id);
-    };
+	const removeOnClick = (e) => {
+		clrList = clrList.filter((x, y) => x !== e.target.id);
+		console.log(e.target.id);
+	};
 
-    $: clrStyle = clrList.map((x) => {
-        return `background: ${x};`;
-    });
+	$: clrStyle = clrList.map((x) => {
+		return `background: ${x};`;
+	});
 
-    let clrVal = '';
+	let clrVal = '';
 
-    const updateClrVal1 = () => {
-        clrVal = '#0f0';
-    };
-    const updateClrVal2 = () => {
-        clrVal = '#00f';
-    };
+	const updateClrVal1 = () => {
+		clrVal = '#0f0';
+	};
+	const updateClrVal2 = () => {
+		clrVal = '#00f';
+	};
 
-    $: bgGradient =
-        clrList.length > 1
-            ? `background-image: linear-gradient(${angle}deg, ${clrList.join(', ')});`
-            : `background-color: ${clrList};`;
+	$: bgGradient =
+		clrList.length > 1
+			? `background-image: linear-gradient(${angle}deg, ${clrList.join(', ')});`
+			: `background-color: ${clrList};`;
 
-    let angle = 270;
+	let angle = 270;
 
-    let speed = 7;
+	let speed = 7;
 
-    $: gradientSpeed = `animation-duration: ${speed}s;`;
+	$: gradientSpeed = `animation-duration: ${speed}s;`;
 
-    let colorInput;
-    let alphaSlider;
-    let colorDisplay;
+	let colorInput;
+	let alphaSlider;
+	let colorDisplay;
 
-    const updateColorDisplay = () => {
-        const selectedColor = colorInput.value;
-        const alphaValue = alphaSlider.value;
-        colorDisplay.style.backgroundColor = selectedColor;
-        colorDisplay.style.opacity = alphaValue;
-    };
+	const updateColorDisplay = () => {
+		const selectedColor = colorInput.value;
+		const alphaValue = alphaSlider.value;
+		colorDisplay.style.backgroundColor = selectedColor;
+		colorDisplay.style.opacity = alphaValue;
+	};
 
-    let myBtn;
+	let myBtn;
 
-    // Set the initial color
+	// Set the initial color
 
-    $: css = `
+	$: css = `
     .animated-background {
         ${bgGradient}
         background-size: 400% 400%;
@@ -92,114 +92,166 @@
         }
     }`;
 
-    const copyFunction = () => {
-        // Copy the text inside the text field
-        navigator.clipboard.writeText(css);
+	const copyFunction = () => {
+		// Copy the text inside the text field
+		navigator.clipboard.writeText(css);
 
-        // Alert the copied text
-        alert('Copied the styles');
-    };
+		// Alert the copied text
+		alert('Copied the styles');
+	};
 
-    // function for the Random button
+	// function for the Random button
 
-    const changeGradient = () => {
-        isRandom = !isRandom;
+	const changeGradient = () => {
+		isRandom = !isRandom;
 
-        // Generate random colors
-        clrList = [getRandomColor(), getRandomColor(), getRandomColor(), getRandomColor()];
-    };
+		// Generate random colors
+		clrList = [getRandomColor(), getRandomColor(), getRandomColor(), getRandomColor()];
+	};
 
-    function getRandomColor() {
-        const letters = '0123456789ABCDEF';
-        let color = '#';
-        for (let i = 0; i < 6; i++) {
-            color += letters[Math.floor(Math.random() * 16)];
-        }
-        return color;
-    }
+	function getRandomColor() {
+		const letters = '0123456789ABCDEF';
+		let color = '#';
+		for (let i = 0; i < 6; i++) {
+			color += letters[Math.floor(Math.random() * 16)];
+		}
+		return color;
+	}
 
-    // A flag to toggle between linear and radial gradients
-    let gradientType: 'linear' | 'angular' | 'radial' = 'linear';
+	// A flag to toggle between linear and radial gradients
+	let gradientType: 'linear' | 'angular' | 'radial' = 'linear';
 
-    const toggleGradientType = () => {
-        if (gradientType === 'linear') {
-            gradientType = 'angular';
-        } else if (gradientType === 'angular') {
-            gradientType = 'radial';
-        } else {
-            gradientType = 'linear';
-        }
-    };
+	const toggleGradientType = () => {
+		if (gradientType === 'linear') {
+			gradientType = 'angular';
+		} else if (gradientType === 'angular') {
+			gradientType = 'radial';
+		} else {
+			gradientType = 'linear';
+		}
+	};
 
-    $: bgGradient =
-        gradientType === 'linear'
-            ? clrList.length > 1
-                ? `background-image: linear-gradient(${angle}deg, ${clrList.join(', ')});`
-                : `background-color: ${clrList};`
-            : gradientType === 'angular'
-            ? clrList.length > 1
-                ? `background-image: conic-gradient(from ${angle}deg, ${clrList.join(', ')});`
-                : `background-color: ${clrList};`
-            : `background-image: radial-gradient(ellipse ${angle}% ${angle}% at center, ${clrList.join(', ')});`;
+	let output;
+
+    let isPaused = false;
+
+	$: if (output) {
+		if (gradientType === 'linear') {
+            isPaused = true;
+			if (clrList.length > 1) {
+				output.style.setProperty(
+					'background-image',
+					`linear-gradient(${angle}deg, ${clrList.join(', ')})`
+                    );
+                output.style.setProperty('background-size', `400% 400%`);
+			} else {
+				bgGradient = `background-color: ${clrList};`;
+			}
+		} else if (gradientType === 'angular') {
+			if (clrList.length > 1) {
+
+                isPaused = false;
+
+				output.style.setProperty(
+					'background-image',
+					`conic-gradient(from var(--angle), ${clrList.join(', ')})`
+				);
+				output.style.setProperty('background-size', `100% 100%`);
+
+				let angle = 0;
+
+			    setInterval(() => {
+                        angle += 1;
+                    angle %= 360;
+					output.style.setProperty('--angle', `${angle}deg`);
+				}, 10);
+			} else {
+				bgGradient = `background-color: ${clrList};`;
+			}
+		} else {
+            isPaused = true;
+            if (clrList.length > 1) {
+				output.style.setProperty(
+					'background-image',
+					`radial-gradient(ellipse ${angle}% ${angle}% at center, ${clrList.join(', ')})`
+				);
+			} else {
+				bgGradient = `background-color: ${clrList};`;
+			}
+		}
+	}
+
+	// $: bgGradient =
+	// 	gradientType === 'linear'
+	// 		? clrList.length > 1
+	// 			? `background-image: linear-gradient(${angle}deg, ${clrList.join(', ')});`
+	// 			: `background-color: ${clrList};`
+	// 		: gradientType === 'angular'
+	// 		? clrList.length > 1
+	// 			? `background-image: conic-gradient(from ${angle}deg, ${clrList.join(', ')});`
+	// 			: `background-color: ${clrList};`
+	// 		: `background-image: radial-gradient(ellipse ${angle}% ${angle}% at center, ${clrList.join(
+	// 				', '
+	// 		  )});`;
 </script>
 
 <Intro heading={data.meta.title} description={data.meta.description} />
 
 <section class="bg-white dark:bg-gray-900">
-    <br />
-    <hr />
+	<br />
+	<hr />
 
-    <div
-        class="color-div py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-12 items-center content-center"
-    >
-        <!-- the color div part -->
+	<div
+		class="color-div py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-12 items-center content-center"
+	>
+		<!-- the color div part -->
 
-        <div
-            class="grid grid-cols-3 sm:grid-cols-6 md:grid-cols-8 lg:grid-cols-10 xl:grid-cols-12 2xl:grid-cols-14 gap-2 justify-center items-center"
-        >
-            {#each clrStyle as clr, i}
-                <div class="relative rounded-lg w-20 h-20">
-                    <button
-                        class="absolute top-[-4px] right-0 rounded-full h-6 w-6 bg-[#B8DBD9] flex justify-center items-center"
-                        on:click={removeOnClick}
-                        bind:this={myBtn}
-                        id={`${clrList[i]}`}
-                    >
-                        <svg
-                            class="cross w-[12px] h-[12px] text-gray-500"
-                            aria-hidden="true"
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 14 14"
-                        >
-                            <path
-                                stroke="currentColor"
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                stroke-width="2"
-                                d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
-                            />
-                        </svg>
-                    </button>
-                    <div class="aspect-square h-[50px] mx-4" style={clr} />
-                </div>
-            {/each}
-        </div>
+		<div
+			class="grid grid-cols-3 sm:grid-cols-6 md:grid-cols-8 lg:grid-cols-10 xl:grid-cols-12 2xl:grid-cols-14 gap-2 justify-center items-center"
+		>
+			{#each clrStyle as clr, i}
+				<div class="relative rounded-lg w-20 h-20">
+					<button
+						class="absolute top-[-4px] right-0 rounded-full h-6 w-6 bg-[#B8DBD9] flex justify-center items-center"
+						on:click={removeOnClick}
+						bind:this={myBtn}
+						id={`${clrList[i]}`}
+					>
+						<svg
+							class="cross w-[12px] h-[12px] text-gray-500"
+							aria-hidden="true"
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 14 14"
+						>
+							<path
+								stroke="currentColor"
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								stroke-width="2"
+								d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+							/>
+						</svg>
+					</button>
+					<div class="aspect-square h-[50px] mx-4" style={clr} />
+				</div>
+			{/each}
+		</div>
 
-        <br />
+		<br />
 
-        <!-- Color Palette part -->
-        <div
-            class="card gap-16 m-4 items-center mx-auto max-w-screen-xl md:grid md:grid-cols-2 overflow-hidden rounded-lg"
-        >
-            <div class="p-8 text-center">
-                <Label class="mt-3 text-2xl">COLOR PALETTE</Label>
-                <br />
-                <Label class="mt-3">Choose color from the box</Label>
-                <br />
-                <div class="color-picker">
-                    <input type="color" id="color-input" bind:value={clrVal3} on:input={updateColorDisplay} />
-                    <!-- <input
+		<!-- Color Palette part -->
+		<div
+			class="card gap-16 m-4 items-center mx-auto max-w-screen-xl md:grid md:grid-cols-2 overflow-hidden rounded-lg"
+		>
+			<div class="p-8 text-center">
+				<Label class="mt-3 text-2xl">COLOR PALETTE</Label>
+				<br />
+				<Label class="mt-3">Choose color from the box</Label>
+				<br />
+				<div class="color-picker">
+					<input type="color" id="color-input" bind:value={clrVal3} on:input={updateColorDisplay} />
+					<!-- <input
                                 type="range"
                                 id="alpha-slider"
                                 min="0"
@@ -208,114 +260,123 @@
                                 value="1"
                                 on:input={updateColorDisplay}
                             /> -->
-                    <div id="color-display" />
-                </div>
+					<div id="color-display" />
+				</div>
 
-                <div class="my-8">
-                    <button class="m-4 w-40 p-4 rounded-lg" on:click={pushArr}><b>+</b> Add color </button>
-                    <br />
-                    <button class="m-4 w-40 p-4 rounded-lg" on:click={changeGradient}> Random Colors </button>
-                    <br />
-                    <button class="m-4 w-40 p-4 rounded-lg" on:click={toggleGradientType}>
-                        {#if gradientType === 'linear'}
-                            Use Angular Gradient
-                        {:else if gradientType === 'angular'}
-                            Use Radial Gradient
-                        {:else}
-                            Use Linear Gradient
-                        {/if}
-                    </button>
-                </div>
+				<div class="my-8">
+					<button class="m-4 w-40 p-4 rounded-lg" on:click={pushArr}><b>+</b> Add color </button>
+					<br />
+					<button class="m-4 w-40 p-4 rounded-lg" on:click={changeGradient}> Random Colors </button>
+					<br />
+					<button class="m-4 w-40 p-4 rounded-lg" on:click={toggleGradientType}>
+						{#if gradientType === 'linear'}
+							Use Angular Gradient
+						{:else if gradientType === 'angular'}
+							Use Radial Gradient
+						{:else}
+							Use Linear Gradient
+						{/if}
+					</button>
+				</div>
 
-                <Label class="mt-3">ANGLE&nbsp;&nbsp;&nbsp;{angle}°</Label>
-                <Range
-                    bind:value={angle}
-                    min="0"
-                    max="360"
-                    on:change={() => {
-                        console.log(angle);
-                    }}
-                />
-                <br />
-                <Label class="mt-3">DURATION&nbsp;&nbsp;&nbsp;{speed}s</Label>
-                <Range
-                    bind:value={speed}
-                    min="1"
-                    max="20"
-                    on:change={() => {
-                        console.log(speed);
-                    }}
-                />
-            </div>
+				<Label class="mt-3">ANGLE&nbsp;&nbsp;&nbsp;{angle}°</Label>
+				<Range
+					bind:value={angle}
+					min="0"
+					max="360"
+					on:change={() => {
+						console.log(angle);
+					}}
+				/>
+				<br />
+				<Label class="mt-3">DURATION&nbsp;&nbsp;&nbsp;{speed}s</Label>
+				<Range
+					bind:value={speed}
+					min="1"
+					max="20"
+					on:change={() => {
+						console.log(speed);
+					}}
+				/>
+			</div>
 
-            <div class="p-8 h-full flex rounded-lg relative output" style={bgGradient + gradientSpeed} />
-        </div>
-        <br />
+			<div
+				class="p-8 h-full flex rounded-lg relative output"
+				id="output"
+				bind:this={output}
+				style={gradientSpeed}
+			/>
+		</div>
+		<br />
 
-        <!-- the text area part -->
-        <div
-            class="card m-4 p-1 bg-gray-100 items-center mx-auto max-w-screen-xl lg:grid rounded-lg relative"
-        >
-            <pre class="whitespace-pre-line p-4">{@html css}</pre>
-            <Copy text={css} on:click={copyFunction} />
-        </div>
-    </div>
+		<!-- the text area part -->
+		<div
+			class="card m-4 p-1 bg-gray-100 items-center mx-auto max-w-screen-xl lg:grid rounded-lg relative"
+		>
+			<pre class="whitespace-pre-line p-4">{@html css}</pre>
+			<Copy text={css} on:click={copyFunction} />
+		</div>
+	</div>
 </section>
 
 <style>
-    /* Add a hover effect to your buttons */
-    button {
-        background-color: #b8dbd9;
-        color: #2f4550;
-        transition: background-color 0.3s ease;
-        cursor: pointer;
-    }
+	/* Add a hover effect to your buttons */
+	button {
+		background-color: #b8dbd9;
+		color: #2f4550;
+		transition: background-color 0.3s ease;
+		cursor: pointer;
+	}
 
-    button::after {
-        background: rgba(0, 0, 0, 0);
-        color: #b8dbd9;
-        transition: all 0.3s ease;
-    }
+	button::after {
+		background: rgba(0, 0, 0, 0);
+		color: #b8dbd9;
+		transition: all 0.3s ease;
+	}
 
-    button:hover {
-        color: #b8dbd9;
-        background-color: black;
-    }
+	button:hover {
+		color: #b8dbd9;
+		background-color: black;
+	}
 
-    button:hover::after {
-        background: rgba(184, 219, 217, 0.3);
-    }
+	button:hover::after {
+		background: rgba(184, 219, 217, 0.3);
+	}
 
-    .card {
-        box-shadow: rgba(0, 0, 0, 0.1) 0 0 0 2px;
-    }
+	.card {
+		box-shadow: rgba(0, 0, 0, 0.1) 0 0 0 2px;
+	}
 
-    :is(.dark .card) {
-        box-shadow: rgba(255, 255, 255, 0.5) 0 0 0 2px;
-    }
+	:is(.dark .card) {
+		box-shadow: rgba(255, 255, 255, 0.5) 0 0 0 2px;
+	}
 
-    .h-full {
-        min-height: 300px;
-    }
+	.h-full {
+		min-height: 300px;
+	}
 
-    .output {
-        background-size: 400% 400%;
-        animation: gradient 15s ease infinite;
-    }
+	#output {
+		--angle: 0deg;
+	}
 
-    @keyframes gradient {
-        0% {
-            background-position: 0% 50%;
-        }
-        50% {
-            background-position: 100% 50%;
-        }
-        100% {
-            background-position: 0% 50%;
-        }
-    }
+	.output {
+		background-size: 400% 400%;
+		animation: gradient 15s ease infinite;
+	}
 
-    .cross {
-        pointer-events: none;
-    }
+	@keyframes gradient {
+		0% {
+			background-position: 0% 50%;
+		}
+		50% {
+			background-position: 100% 50%;
+		}
+		100% {
+			background-position: 0% 50%;
+		}
+	}
+
+	.cross {
+		pointer-events: none;
+	}
 </style>
