2ec708b9e9e6c39483b4644be43e26ec186efe76
Rashmi-2002 <rashminanajkar@gmail.com>
2023-10-25 18:43:30 +0530

    Updated Text Truncator Tool

--- src/routes/(tools)/text-truncator/+page.svelte
+++ src/routes/(tools)/text-truncator/+page.svelte
@@ -5,18 +5,29 @@
 	import Intro from '$lib/Intro.svelte';
 
 	export let data;
+	let text = ''; 
+    let direction='end'; 
+    let limit = 10; 
+    let output = ''; 
+   function updateTruncatedText() {
+if (text.length > limit) {
+      if (direction === 'end') {
+        output = text.slice(0, limit) + '...';
+      } else {
+        output = '...' + text.slice(-limit);
+      }
+    } else {
+      output = text; 
+    }
+  }
+  function onDirectionChange(event) {
+    direction = event.target.value;
+	updateTruncatedText()
+  }
 
-	var len = 10;
-
-	var output;
-
-	function generateoutput(input) {
-		if (input.target.value.length > len) {
-			output = input.target.value.slice(0, len) + '...';
-		} else {
-			output = input.target.value;
-		}
-	}
+  $: {
+    updateTruncatedText();
+  }
 	
 	function copyText() {
 		if (output.length > 0) {
@@ -31,7 +42,7 @@
 
 	function downloadText() {
 		if (output.length > 0) {
-			var filename = "DevStar.txt";
+			var filename = "text-truncator-output.txt";
 			var blob = new Blob([output], { type: 'text/plain' });
 			var url = window.URL.createObjectURL(blob);
 			
@@ -50,7 +61,7 @@
 		if (output.length > 0) {
 			const doc = new jsPDF();
 			doc.text(output, 20, 20);
-			doc.save('DevStaroutput.pdf');
+			doc.save('text-truncator-output.pdf');
 		}
 	}
 
@@ -61,24 +72,30 @@
 <section class="bg-white dark:bg-gray-900">
 	<div class="py-8 px-4 mx-auto max-w-screen-xl lg:px-12">
 		<div class="card p-8 relative items-center mx-auto max-w-screen-xl overflow-hidden rounded-lg">
-
+			<label for="direction" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Truncate from:</label>
+            <div class="rounded-lg overflow-hidden bg-gray-50 border border-gray-300" id="tarea1">
+				<select id="direction" bind:value={direction} on:change={onDirectionChange}  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+					<option value="end">End</option>
+					<option value="start">Start</option>
+				</select>	
+			</div><br>
 			<div>
-				<label for="" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Number of Characters</label>
+				<label for="" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Number of Characters:</label>
 				<div class="rounded-lg overflow-hidden bg-gray-50 border border-gray-300">
-					<input type="number" id="" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-					bind:value={len}>
+					<input type="number" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+					bind:value={limit} on:input={updateTruncatedText}>
 				</div>
 			</div>
 
 			<div class="mt-3 gap-2 items-center mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 overflow-hidden" id="boxarea">
 
 				<div class="rounded-lg overflow-hidden bg-gray-50 border border-gray-300" id="tarea1">
-					<textarea placeholder="Enter Text" id="textbox" rows="8" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-					on:input={generateoutput}/>
+					<textarea bind:value={text} placeholder="Enter Text" id="textbox" rows="8" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+					on:input={updateTruncatedText}/>
 				</div>
 
 				<div class="rounded-lg overflow-hidden bg-gray-50 border border-gray-300" id="tarea2">
-					<textarea placeholder="Result" id="textbox" rows="8" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+					<textarea readonly placeholder="Result" id="textbox" rows="8" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 					bind:value={output}/>
 				</div>
 
