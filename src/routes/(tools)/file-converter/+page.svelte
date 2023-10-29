a9c22ad5a7448e6375f132774d17d85e08286b67
Mohammad Adil <mohammadadil3292@gmail.com>
2023-10-29 23:24:10 +0530

    added preview feature and merged all type of supported file conversion

--- src/routes/(tools)/file-converter/+page.svelte
+++ src/routes/(tools)/file-converter/+page.svelte
@@ -1,27 +1,47 @@
 <script lang="ts">
 	import { Label } from 'flowbite-svelte';
 	import Intro from '$lib/Intro.svelte';
-    import {fileFormats} from './formats'
+
+	let fileFormats = {
+		image: {
+			name: 'Image Formats',
+			outputFormat: ['bmp', 'fax', 'g3', 'gif', 'hdr', 'ico', 'jp2', 'jpeg', 'jpg', 'map', 'mng', 'mtv', 'odd', 'pal', 'palm', 'pict', 'png', 'pnm', 'svgz', 'webp']
+		},
+		audio: {
+			name: 'Audio Formats',
+			outputFormat: ['aac', 'ac3', 'aiff', 'amr', 'ape', 'caf', 'dts', 'flac', 'm4a', 'mp3', 'ogg', 'wav', 'wma']
+		},
+		video: {
+			name: 'Video Formats',
+			outputFormat: ['3g2', '3gp', 'amv', 'asf', 'avi', 'dvd', 'm4v', 'mov', 'mp4', 'mpeg', 'mpg', 'ogv', 'webm', 'wmv']
+		}
+	};
 
 	let selectedFormat = ''; 
 	let inputFile, fileName='', extension='';
+    let outputFile='', fileURL='';
 	let supportedFormats = fileFormats;
-    let outputFile='';
 
 	const getFileFormat = (e) =>{
         outputFile='';
 		inputFile = e.target.files[0];
+		console.log(inputFile);
 		if(inputFile['name'].length){
 			let file = inputFile['name'].split('.');
+			let found = false;
 			fileName = file[0];
 			extension = file[1];
 			for(let format in fileFormats){
 				fileFormats[format]['outputFormat'].forEach((ext,key)=>{
 					if(extension===ext){
 						supportedFormats = fileFormats[format];
-						return;
+						console.log(supportedFormats);
+						found = true;
 					}
 				})
+			}
+			if(found===false){
+				supportedFormats = {name:'Not Supported', outputFormat:[]};
 			}
 		}else{
 			fileName='';
@@ -29,20 +49,26 @@
 		}
 	}
 
-const convertFormat = () => {
-    outputFile = `${fileName}.${selectedFormat}`;
-};
+	const convertFormat = () => {
+		outputFile = `${fileName}.${selectedFormat}`;
+	};
 
+	const previewFile = () =>{
+		const convertedBlob = new Blob([inputFile], { type: inputFile.type });
+		convertedBlob.name = outputFile;
+		fileURL = URL.createObjectURL(convertedBlob);
+		window.open(fileURL,'_blank');
+	}
 
 	const downloadFile = async () => {
-    const convertedBlob = new Blob([inputFile], { type: inputFile.type });
-    convertedBlob.name = outputFile;
-    const downloadLink = document.createElement('a');
-    downloadLink.href = URL.createObjectURL(convertedBlob);
-    downloadLink.download = outputFile;
-    document.body.appendChild(downloadLink);
-    downloadLink.click();
-    document.body.removeChild(downloadLink);
+		const convertedBlob = new Blob([inputFile], { type: inputFile.type });
+		convertedBlob.name = outputFile;
+		const downloadLink = document.createElement('a');
+		downloadLink.href = URL.createObjectURL(convertedBlob);
+		downloadLink.download = outputFile;
+		document.body.appendChild(downloadLink);
+		downloadLink.click();
+		document.body.removeChild(downloadLink);
 	}
 	export let data;
 </script>
@@ -63,10 +89,10 @@ const convertFormat = () => {
 				<div class="flex items-center">
 					<select bind:value={selectedFormat} class="rounded-lg border border-gray-400 dark:border-white w-full" required>
 					<option value="">Select Output Format</option>
-					{#if fileName==='' && extension===''}
-						{#each Object.keys(fileFormats) as fileType}
-							<option value={fileType} disabled class="text-center font-bold">*******{fileFormats[fileType].name}*******</option>
-							{#each fileFormats[fileType].outputFormat as val}
+					{#if fileName===''}
+						{#each Object.keys(supportedFormats) as fileType}
+							<option value={fileType} disabled class="text-center font-bold">*******{supportedFormats[fileType].name}*******</option>
+							{#each supportedFormats[fileType].outputFormat as val}
 								<option value={val}>{val}</option>
 							{/each}
 						{/each}
@@ -94,7 +120,7 @@ const convertFormat = () => {
 			<div class="rounded-lg grid grid-cols-1 lg:grid-cols-3  gap-2 lg:gap-16  m-8  overflow-hidden">
 				<div class="py-2 px-8 bg-white border border-gray-200 lg:col-span-2 ">{outputFile} </div>
 				<div class="flex items-center justify-center ">
-					<button class="bg-green-700 border text-white p-2 rounded-lg w-24">Preview</button>
+					<button class="bg-green-700 border text-white p-2 rounded-lg w-24" on:click={previewFile}>Preview</button>
 					<button class="bg-blue-700 border text-white p-2 rounded-lg w-24 mx-2" on:click={downloadFile}>Download</button>
 				</div>
 			</div>
