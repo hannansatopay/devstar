e15fedc702565a5aa5e6eafdd9e5615e4114754c
sahilbalia <sahilbalia03@gmail.com>
2023-10-30 19:22:26 +0530

    Update files

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
-import { isoImport } from 'vite-plugin-iso-import';
+
 export default defineConfig({
-
-plugins: [sveltekit(), isoImport()],
-
+	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
 });
-
-
-
