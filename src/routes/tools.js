8b99731b2bf4f40270d45a44665d7fe1ca64d7fd
Hannan Satopay <sathannan@hotmail.com>
2024-05-10 18:11:16 +0530

    merge fixes

--- src/routes/tools.js
+++ src/routes/tools.js
@@ -427,7 +427,8 @@ export let tools = {
         name: 'compound calculator',
         icon: 'code',
         link: '/compound',
-=======
+		description:''
+	},
     'amortiztion-calculator': {
         name: 'amortiztion-calculator',
         icon: 'spider-web',
@@ -439,113 +440,19 @@ export let tools = {
         icon: 'spider-web',
         link: '/discount-calculator',
         description: "The term discount can be used to refer to many forms of reduction in the price of a good or service. The two most common types of discounts are discounts in which you get a percent off."
-
-    'password-generator': {
-        name: 'Password Generator',
-        icon: 'key',
-        link: '/password-generator',
-        description: "The Password Generator is a reliable and secure tool that enables developers to easily generate strong and unique passwords for their applications. With customizable options, such as password length, character types (uppercase, lowercase, numbers, symbols), and exclusion of ambiguous characters, this tool ensures the creation of robust passwords that meet the highest security standards. Simplify the process of password generation and strengthen the security of your applications with this convenient tool."
-    },
-    'image-color-picker': {
-        name: 'Image Color Picker',
-        icon: 'palette',
-        link: '/image-color-picker',
-        description: "The Image Color Picker is a versatile tool that allows developers to extract color information from images effortlessly. With a simple interface, you can upload an image and identify specific colors by selecting points on the image. The tool provides the corresponding color codes (e.g., HEX, RGB, HSL) for easy integration into your projects. Streamline your color selection process and enhance your application's visual appeal with this efficient color picker."
-    },
-    'dummy-text-generator': {
-        name: 'Dummy Text Generator',
-        icon: 'text-size',
-        link: '/dummy-text-generator',
-        description: "The Dummy Text Generator is an essential tool for developers seeking to populate their applications with realistic placeholder text. This tool generates random lorem ipsum text or customizable dummy text based on desired parameters such as word count, paragraph count, and language. Seamlessly integrate this tool into your development workflow to quickly fill your application prototypes or design mockups with meaningful placeholder text."
-    },
-    'random-image-generator': {
-        name: 'Random Image Generator',
-        icon: 'shuffle',
-        link: '/random-image-generator',
-        description: "The Random Image Generator is a dynamic tool that enables developers to generate random images on the fly. Whether you need placeholder images for testing or want to add visual elements to your applications, this tool can generate a wide variety of random images based on different categories, dimensions, and styles. Utilize this tool to add visual appeal and realism to your applications effortlessly."
-    },
-    'screen-video-recorder': {
-        name: 'Screen Video Recorder',
-        icon: 'display',
-        link: '/screen-video-recorder',
-        description: "The Screen Video Recorder is a handy tool designed for developers to capture and record high-quality videos of their screen activities. Whether you need to create software demos, tutorial videos, or bug reports, this tool provides a user-friendly interface to record your screen with customizable options, including video resolution, audio settings, and capture area selection. Enhance your documentation and communication efforts by easily recording and sharing your screen activities."
-    },
-    'unit-converter': {
-        name: 'Unit Converter',
-        icon: 'arrow-right-arrow-left',
-        link: '/unit-converter',
-        description: "The Unit Converter is a comprehensive tool that simplifies the conversion of various units for developers. With support for a wide range of unit types, such as length, weight, temperature, time, and more, this tool allows you to convert values seamlessly between different measurement systems. Save time and effort by integrating this versatile unit converter into your development toolkit for quick and accurate unit conversions."
-    },
+	},
     'rent-calculator': {
         name: 'Rent Calculator',
         icon: 'arrow-right-arrow-left',
         link: '/rent-calculator',
         description: "The Rent Calculator is a comprehensive tool that simplifies the conversion of various units for developers. With support for a wide range of unit types, such as length, weight, temperature, time, and more, this tool allows you to convert values seamlessly between different measurement systems. Save time and effort by integrating this versatile unit converter into your development toolkit for quick and accurate unit conversions."
     },
-    'image-base64-encode': {
-        name: 'Image Base64 Encode',
-        icon: 'folder-image',
-        link: '/image-base64-encode',
-        description: "The Image Base64 Encode tool enables developers to convert image files into Base64-encoded strings effortlessly. By simply uploading an image file, this tool converts the image into a Base64-encoded representation, which can be easily integrated into HTML, CSS, or JavaScript code. Integrate this tool into your development workflow to efficiently handle image data and optimize its usage in various web applications."
-    },
-    'image-base64-decode': {
-        name: 'Image Base64 Decode',
-        icon: 'file-image',
-        link: '/image-base64-decode',
-        description: "The Image Base64 Decode tool offers developers an efficient way to decode Base64-encoded image data back into image files. This tool accepts Base64-encoded strings and converts them into their corresponding image file formats, enabling developers to handle and manipulate image data seamlessly. Simplify your image data processing workflow with this tool and ensure efficient utilization of Base64-encoded images in your applications."
-    },
-    'pomodoro': {
-        name: 'Pomodoro',
-        icon: 'clock-desk',
-        link: '/pomodoro',
-        description: "The Pomodoro tool is a productivity-enhancing feature designed specifically for developers. Based on the popular Pomodoro Technique, this tool helps you manage your time effectively by breaking your work into focused intervals, typically 25 minutes long, followed by short breaks. This systematic approach aids in maintaining concentration, combating procrastination, and boosting productivity. Integrate this tool into your workflow to optimize your work habits and accomplish tasks more efficiently."
-    },
-    'advice-generator': {
-        name: 'Advice Generator',
-        icon: 'message-captions',
-        link: '/advice-generator',
-        description: "The Advice Generator is a valuable tool that provides developers with inspirational and practical advice to overcome challenges and improve their work. By generating random advice snippets, this tool helps developers find guidance and inspiration. Access a wealth of advice easily with this tool and elevate your mindset."
-    },
-    'sample-files': {
-        name: 'Sample Files',
-        icon: 'files',
-        link: '/sample-files',
-        description: "The Sample Files tool offers developers a collection of pre-generated files, such as text documents, images, audio files, and more, for testing and development purposes. These files can serve as placeholders or samples to ensure compatibility, verify functionality, or assist in creating realistic test scenarios. Access this extensive library of sample files to streamline your testing and development processes."
-    },
-    'css-animator': {
-        name: 'CSS Animator',
-        icon: 'film-simple',
-        link: '/css-animator',
-        description: "The CSS Animator is a powerful tool that simplifies the creation of engaging and interactive CSS animations for developers. With an intuitive interface, you can generate CSS animations by defining keyframes, durations, delays, and easing functions. This tool eliminates the need for manual coding and enables developers to bring their designs to life with captivating CSS animations easily."
-    },
-    'device-details': {
-        name: 'Device Details',
-        icon: 'laptop-mobile',
-        link: '/device-details',
-        description: "The Device Details tool provides developers with essential information about user devices, including browser specifications, screen resolution, operating system details, and more. By integrating this tool into your applications, you can gather valuable insights about your users' devices, allowing you to optimize user experiences, ensure compatibility, and tailor your content or features accordingly."
-    },
-    'whatsapp-api-generator': {
-        name: 'WhatsApp API Generator',
-        icon: 'whatsapp',
-        link: '/whatsapp-api-generator',
-        description: "The WhatsApp API Generator is a specialized tool that simplifies the process of generating API integration code for the WhatsApp Me feature. With this tool, developers can quickly create the necessary code snippets to enable users to initiate conversations with a specific phone number on WhatsApp. This generator streamlines the integration of the WhatsApp Me functionality into your applications, allowing users to reach out and engage with your business or service effortlessly. Enhance user communication and engagement by utilizing this powerful API generator for WhatsApp integration."
-    },
-    'robots-txt-generator': {
-        name: 'Robots.txt Generator',
-        icon: 'spider-web',
-        link: '/robots-txt-generator',
-
-        description: "The Robots.txt Generator is a must-have tool for developers to create the robots.txt file, which controls search engine crawlers' access to specific pages or directories on a website. This tool offers a user-friendly interface to define crawling rules, including allow and disallow directives, to ensure optimal search engine indexing and prevent unwanted content from being indexed. Create and customize your robots.txt file efficiently with this generator to enhance your website's search engine optimization (SEO) efforts."
-    },
-
-
     'BMI': {
         name: 'BMI',
         icon: 'spider-web',
         link: '/random-image-generator',
         description: "The Robots.txt Generator is a must-have tool for developers to create the robots.txt file, which controls search engine crawlers' access to specific pages or directories on a website. This tool offers a user-friendly interface to define crawling rules, including allow and disallow directives, to ensure optimal search engine indexing and prevent unwanted content from being indexed. Create and customize your robots.txt file efficiently with this generator to enhance your website's search engine optimization (SEO) efforts."
     },
-
     'Simple Interest': {
         name: 'Simple Interest',
         icon: 'spider-web',
@@ -557,8 +464,5 @@ export let tools = {
         icon: 'currency-dollar',
         link:'/cash-back-or-low-interest-calculator',
         description:'Auto manufacturers may offer either a cash back rebate or a low interest rate with a car purchase. Very often, these offers are mutually exclusive. Use the calculator to find out which of the two is the better offer.'
-
-
-
     }
 };
