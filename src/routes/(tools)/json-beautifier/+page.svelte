fd7c158a5a8cb42a68b5f504ad1ddc53bce46d6c
sarathp22 <sarathp22@gmail.com>
2024-06-26 14:17:22 +0530

    html template added
    html template added

--- src/routes/(tools)/json-beautifier/+page.svelte
+++ src/routes/(tools)/json-beautifier/+page.svelte
@@ -4,6 +4,34 @@
 
 <div class="card gap-16 items-center mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 overflow-hidden rounded-lg">
 	<!-- Add tool here -->
+	<div class="bg-teal-500">
+		<header class="bg-slate-900 py-4 sticky top-0 z-10">
+		  <div class="px-4">
+				<h1 class="text-2xl text-white font-bold font-['Comic_Sans_MS']">JSON Formatter</h1>
+		  </div>
+		</header>
+	
+	  <div class="grid grid-cols-1 md:grid-cols-3">
+		<div class="h-screen mt-4">
+		  <div class="bg-white h-screen">
+			JSON here
+		  </div>
+		</div>
+		<div class="mx-auto my-16">
+		  <button class="block w-44 mt-3 bg-teal-500 hover:bg-teal-700 text-2xl border-4 border-white text-white font-bold py-2 px-4 rounded">Upload Data</button>
+		  <button class="block w-44 mt-3 bg-teal-500 hover:bg-teal-700 text-2xl border-4 border-white text-white font-bold py-2 px-4 rounded">Validate</button>
+		  <button class="block w-44 mt-3 bg-teal-500 hover:bg-teal-700 text-2xl border-4 border-white text-white font-bold py-2 px-4 rounded">Format/ Beautify</button>
+		  <button class="block w-44 mt-3 bg-teal-500 hover:bg-teal-700 text-2xl border-4 border-white text-white font-bold py-2 px-4 rounded">Download</button>
+		  
+		</div>
+		<div class="h-screen mt-4">
+		  <div class="bg-white h-screen">
+			Formatted JSON
+		  </div>
+		</div>
+	
+	  </div>
+	  </div>
 </div>
 
 <style>
