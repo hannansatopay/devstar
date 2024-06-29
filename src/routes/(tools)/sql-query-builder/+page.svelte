036b03cef3b9c30b35606d09a63ccfe291567e94
Atharva Sawant <84018291+Satharva2004@users.noreply.github.com>
2024-06-29 17:34:22 +0530

    atharva UI

--- src/routes/(tools)/sql-query-builder/+page.svelte
+++ src/routes/(tools)/sql-query-builder/+page.svelte
@@ -1,11 +1,40 @@
 <script>
-
+  // Your script content here
 </script>
 
-<div class="card gap-16 items-center mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 overflow-hidden rounded-lg">
-	<!-- Add tool here -->
+<div class="justify-center font-sans mt-4 overflow-hidden rounded-lg">
+  <div class="mt-5 ml-20">
+    <label
+      for="message"
+      class="block mb-4 text-3xl font-extrabold tracking-tight text-gray-900 dark:text-white"
+    >
+      <span class="flex items-center">
+        Enter Prompt
+        <img
+          src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Travel%20and%20places/Rocket.png"
+          alt="Rocket"
+          width="35"
+          height="35"
+          class="ml-3"
+        />
+      </span>
+    </label>
+    <textarea
+      id="message"
+      rows="4"
+      class="block p-2.5 md:w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-100 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-900 dark:border-gray-400 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+      placeholder="Description of the query to generate..."
+    ></textarea>
+    <div class="mb-2 mt-4">
+      <button
+        type="button"
+        class="text-white tracking-tight bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-4 py-2.5 text-center me-2 mb-2 ml-1"
+        >Generate</button
+      >
+    </div>
+  </div>
 </div>
 
 <style>
-
+  /* Your style content here */
 </style>
