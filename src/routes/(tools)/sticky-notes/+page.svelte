61fa24c17525ae876f256dd665a3406e7c55d5f4
ChhayaSharma21 <chhayasharma117@gmail.com>
2024-07-01 18:24:23 +0530

    Button_Change

--- src/routes/(tools)/sticky-notes/+page.svelte
+++ src/routes/(tools)/sticky-notes/+page.svelte
@@ -105,8 +105,10 @@
                     
                     <button
                         type="submit"
-                        class="absolute bottom-2 right-2 px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600"
-                    >Save</button>
+                       ><svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 11.917 9.724 16.5 19 7.5"/>
+                      </svg>
+                      </button>
                     
                 </form>
             </div>
