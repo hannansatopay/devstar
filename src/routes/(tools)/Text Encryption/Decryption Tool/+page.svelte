a9ec560c65026102c0a6fdf4454b7b468196a51e
kaneezfatima19 <kaneezfatimabohra@gmail.com>
2024-07-08 18:15:05 +0530

    Kaneez-Fatima
    Encryption(JavaScript)

--- src/routes/(tools)/Text Encryption/Decryption Tool/+page.svelte
+++ src/routes/(tools)/Text Encryption/Decryption Tool/+page.svelte
@@ -1,11 +1,22 @@
 <script>
+  import { encrypt, decrypt } from '../lib/encryption';
 
+  let textToEncrypt = '';
+  let password = '';
+  let encryptedText = '';
+
+  function handleEncrypt() {
+    encryptedText = encrypt(textToEncrypt, password);
+  }
+
+  function handleDecrypt() {
+    textToEncrypt = decrypt(encryptedText, password);
+  }
 </script>
 
-<div class="card gap-16 items-center mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 overflow-hidden rounded-lg">
-	<!-- Add tool here -->
-</div>
-
-<style>
-
-</style>
+<input bind:value={textToEncrypt} type="text" placeholder="Text to encrypt" />
+<input bind:value={password} type="password" placeholder="Password" />
+<button on:click={handleEncrypt}>Encrypt</button>
+<button on:click={handleDecrypt}>Decrypt</button>
+<p>Encrypted Text: {encryptedText}</p>
+<p>Decrypted Text: {textToEncrypt}</p>
--- svelte.config.js
+++ svelte.config.js
@@ -7,8 +7,17 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 const config = {
 	preprocess: [vitePreprocess({})],
 	kit: {
+	    
 		adapter: adapter({ out: 'build' })
 	}
 };
 
-export default config;
+/**const config = {
+	kit: {
+	  // hydrate the <div id="svelte"> element in src/app.html
+	  target: '#svelte',
+	  adapter: adapter()
+	}
+  };**/
+  
+  export default config;
