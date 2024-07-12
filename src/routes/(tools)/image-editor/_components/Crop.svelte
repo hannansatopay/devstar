6d061cb4d6185e088f76b461ca81a6ab3b0b1dd2
harshith1019 <harshith.1019@gmail.com>
2024-07-12 12:51:08 +0530

    dummy

--- src/routes/(tools)/image-editor/_components/Crop.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-    export let image;
-</script>
-
-<div class="flex flex-col items-center">
-    <p class="mb-4">Crop feature coming soon!</p>
-    <!-- svelte-ignore a11y-img-redundant-alt -->
-    <img src={image} alt="Image to crop" class="max-w-full max-h-screen" />
-</div>
