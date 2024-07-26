7fa154de712b6ee1bf365d2b62b0329ae7aeca1d
Soumya Ranjan Tripathy <sonutripathy36@gmail.com>
2024-07-27 02:35:50 +0530

    Soumya
    Sonu

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,33 @@
-// import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import adapter from '@sveltejs/adapter-node';
-
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import preprocess from 'svelte-preprocess'
+import { resolve } from 'path'
+import vercel from '@sveltejs/adapter-vercel'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: [vitePreprocess({})],
-	kit: {
-		adapter: adapter({ out: 'build' })
-	}
-};
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: [
+    preprocess({
+      postcss: true,
+    }),
+  ],
 
-export default config;
+  kit: {
+    adapter: vercel(),
+
+    prerender: {
+      enabled: false,
+    },
+
+    vite: {
+      resolve: {
+        alias: {
+          $src: resolve('./src'),
+          $components: resolve('./src/components'),
+        },
+      },
+    },
+  },
+}
+
+export default config
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,72 +1,35 @@
-/** @type {import('tailwindcss').Config}*/
-const config = {
-	content: [
-		'./src/**/*.{html,js,svelte,ts}',
-		'./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'
-	],
+const colors = require('tailwindcss/colors');
+const defaultTheme = require('tailwindcss/defaultTheme');
 
-	theme: {
-		extend: {
-			colors: {
-				primary: {
-					50: '#eff6ff',
-					100: '#dbeafe',
-					200: '#bfdbfe',
-					300: '#93c5fd',
-					400: '#60a5fa',
-					500: '#3b82f6',
-					600: '#2563eb',
-					700: '#1d4ed8',
-					800: '#1e40af',
-					900: '#1e3a8a',
-					950: '#172554'
-				}
-			},
-			backgroundColor: {
-				'custom-blue': '#DCF5F9',
-				'custom-yellow': '#ffdb58'
-			}
-		},
-		fontFamily: {
-			body: [
-				'Inter',
-				'ui-sans-serif',
-				'system-ui',
-				'-apple-system',
-				'system-ui',
-				'Segoe UI',
-				'Roboto',
-				'Helvetica Neue',
-				'Arial',
-				'Noto Sans',
-				'sans-serif',
-				'Apple Color Emoji',
-				'Segoe UI Emoji',
-				'Segoe UI Symbol',
-				'Noto Color Emoji'
-			],
-			sans: [
-				'Inter',
-				'ui-sans-serif',
-				'system-ui',
-				'-apple-system',
-				'system-ui',
-				'Segoe UI',
-				'Roboto',
-				'Helvetica Neue',
-				'Arial',
-				'Noto Sans',
-				'sans-serif',
-				'Apple Color Emoji',
-				'Segoe UI Emoji',
-				'Segoe UI Symbol',
-				'Noto Color Emoji'
-			]
-		}
-	},
-
-	plugins: [require('flowbite/plugin')],
-	darkMode: 'class'
+module.exports = {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        gray: colors.zinc,
+        primary: {
+          DEFAULT: 'var(--color-primary)',
+          clamped: 'var(--color-primary-clamped)',
+          complement: 'var(--color-complement)',
+          text: 'var(--color-primary-text)',
+        },
+      },
+      fontFamily: {
+        sans: ['DM Sans', ...defaultTheme.fontFamily.sans],
+        mono: ['JetBrains Mono', ...defaultTheme.fontFamily.sans],
+      },
+      animation: {
+        'spin-reverse': 'spin-reverse 1s linear infinite',
+      },
+      keyframes: {
+        'spin-reverse': {
+          to: {
+            transform: 'rotate(-360deg)',
+          },
+        },
+      },
+    },
+  },
+  plugins: [],
 };
-
-module.exports = config;
