ae2b7a802cfabaae425687460d2baf057eb5773a
Soumya Ranjan Tripathy <sonutripathy36@gmail.com>
2024-07-29 00:52:44 +0530

    Soumya
    Sonu

--- src/app.d.ts
+++ src/app.d.ts
@@ -3,8 +3,5 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare namespace App {
-	// interface Locals {}
-	// interface Platform {}
-	// interface Session {}
-	// interface Stuff {}
+	
 }
